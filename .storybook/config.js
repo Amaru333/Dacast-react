356a3d01a740e8e20f33fe32d4cbfe74b5d6c6e7
Quentin Benyahia <quentinbenyahia@gmail.com>
2019-09-16 14:23:03 +0100

    config storybook and small exempel

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,14 +1,17 @@
 import { addParameters, configure } from '@storybook/react';
 import { themes } from '@storybook/theming';
+const { addDecorator } = require('@storybook/react');
+const { withPropsTable } = require('storybook-addon-react-docgen');
+
 // automatically import all files ending in *.stories.js
 const req = require.context('../src/stories', true, /.stories.tsx$/);
 function loadStories() {
     req.keys().forEach(req);
 }
-
+addDecorator(withPropsTable);
 addParameters({
     options: {
-      theme: themes.dark,
+      theme: themes.normal,
     },
 });
 
--- src/stories/button.stories.tsx
+++ src/stories/button.stories.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import {ButtonApp} from '../components/ButtonApp/ButtonApp'
 
 storiesOf('Welcome', module).add('to Storybook', () => <React.Fragment> Yo </React.Fragment> );
 
-storiesOf('Button', module)
-  .add('with text2', () => <React.Fragment> Some random Button here </React.Fragment>);
+storiesOf('Buttons', module)
+  .add('Primary button', () => <ButtonApp onClick={action("button-click")}  size="small" type="primary"   > Button Primary  </ButtonApp> );
