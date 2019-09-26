bddb50db5bdd05c2d3c7d9ad221f695bdbed321a
Quentin Benyahia <quentinbenyahia@gmail.com>
2019-09-26 11:57:58 +0100

    change to 32px

--- src/components/FormsComponents/Modal/Modal.tsx
+++ src/components/FormsComponents/Modal/Modal.tsx
@@ -99,7 +99,7 @@ const OverlayStyle = styled.div<{ opened: boolean }>`
 //Should be fixed ...
 const ModalContainerStyle = styled.div<ModalProps>`
   box-sizing: border-box;
-  padding: 24px;
+  padding: 24px 24px 32px 24px;
   width: ${props => (props.size === "small" ? "400px" : "600px")};
   border-radius: ${props => props.theme.borderRadius};
   box-shadow: 0px 4px 4px rgba(34, 47, 62, 0.2);
