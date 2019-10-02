1ddb8d9b79dab2fdb82516d9755431647f517697
Jake Napper <jake.napper@dacast.com>
2019-10-02 15:31:05 +0100

    integrated hooks with toggle

--- src/components/Toggle/toggle.tsx
+++ src/components/Toggle/toggle.tsx
@@ -2,29 +2,17 @@ import * as React from "react";
 import { ToggleLabelStyle, ToggleWrapperStyle, ToggleStyle, LabelTextStyle} from "./ToggleStyle"
 import { ToggleProps } from './ToggleType';
 
+export const Toggle: React.FC<ToggleProps> = (props) => {
 
-export class Toggle extends React.Component<ToggleProps> {
-    constructor(props: ToggleProps) {
-        super(props);
-    }
-
-    render() {
-        
-
-        return (
-           <ToggleWrapperStyle {...this.props}>
-               <ToggleStyle id={this.props.label} type="checkbox" />
-                <ToggleLabelStyle htmlFor={this.props.label}>
-                    <LabelTextStyle color="gray-1" size={16} weight="med">{this.props.label}</LabelTextStyle>
-                </ToggleLabelStyle>
-           </ToggleWrapperStyle> 
-        );
-    }
-
-   
+    return (
+        <ToggleWrapperStyle {...props}>
+            <ToggleStyle id={props.label} type="checkbox" />
+            <ToggleLabelStyle htmlFor={props.label}>
+                <LabelTextStyle color="gray-1" size={16} weight="med">{props.label}</LabelTextStyle>
+            </ToggleLabelStyle>
+        </ToggleWrapperStyle> 
+            );
 
 }
-
-
 
   
