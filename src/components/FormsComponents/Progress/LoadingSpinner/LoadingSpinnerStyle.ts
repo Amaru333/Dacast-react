d95d0d65c6b5a9b9546136c167ae3f428762079f
Clement <clementdacast@Clements-MacBook-Pro.local>
2019-10-02 13:21:03 +0100

    Changing the spinner's style

--- src/components/FormsComponents/Progress/LoadingSpinner/LoadingSpinnerStyle.ts
+++ src/components/FormsComponents/Progress/LoadingSpinner/LoadingSpinnerStyle.ts
@@ -13,23 +13,85 @@ const spin = keyframes`
 
 export const LoadingSpinnerStyle = styled.button<LoadingSpinnerProps>`
   
-  border: 16px solid #f3f3f3;
+  /* border: 7px solid #f3f3f3;
   border-radius: 50%;
-  border-top: 16px solid ${props => props.color};
-  width: 120px;
-  height: 120px;
+  border-top: 7px solid ${props => props.theme.colors[props.color]};
+  border-right: 7px solid ${props => props.theme.colors[props.color]};
+  width: 72px;
+  height: 72px;
 
   animation: ${spin} 2s linear infinite;
   ${props => (props.size == "small") && css`
-    height: 60px;
-    width: 60px;
-    border: 8px solid #f3f3f3;
-    border-top: 8px solid ${props.color};
+    height: 32px;
+    width: 32px;
+    border: 3px solid #f3f3f3;
+    border-top: 3px solid ${props.theme.colors[props.color]};
+    border-right: 3px solid ${props.theme.colors[props.color]};
   `}
   ${props => (props.size == "medium") && css`
-    height: 90px;
-    width: 90px;
-    border: 12px solid #f3f3f3;
-    border-top: 12px solid ${props.color};
+    height: 48px;
+    width: 48px;
+    border: 5px solid #f3f3f3;
+    border-top: 5px solid ${props.theme.colors[props.color]};
+    border-right: 5px solid ${props.theme.colors[props.color]};
+  `} */
+  border-style: solid;
+  border-color: ${props => props.theme.colors[props.color]} ${props => props.theme.colors[props.color]} 
+  ${props => props.theme.colors["gray-9"]} ${props => props.theme.colors["gray-9"]};
+  border-radius: 50%;
+  animation: ${spin} 1.5s infinite;
+  position: relative;
+  border-width: 7px;
+  width: 72px;
+  height: 72px;
+
+  ${props => (props.size == "medium") && css`
+    border-width: 5px;
+    width: 48px;
+    height: 48px;
   `}
+  ${props => (props.size == "small") && css`
+    border-width: 3px;
+    width: 32px;
+    height: 32px;
+  `}
+  &:before, &:after {
+    content: '';
+    width: 7px;
+    height: 7px;
+    border-radius: 50%;
+    background: ${props => props.theme.colors[props.color]};
+    position: absolute;
+    ${props => (props.size == "medium") && css`
+    width: 4.5px;
+    height: 4.5px;
+  `}
+  ${props => (props.size == "small") && css`
+    width: 3px;
+    height: 3px;
+  `}
+  } 
+  &:before {
+    top: 2px;
+    left: 3px;
+    ${props => (props.size == "medium") && css`
+    top: 1.5px;
+    left: 1px;
+  `}
+  ${props => (props.size == "small") && css`
+    top: 0.5px;
+    left: 1.5px;
+  `}
+  } 
+  &:after {
+    bottom: 2px;
+    left: 48px;
+    ${props => (props.size == "medium") && css`
+    left: 32px;
+  `}
+  ${props => (props.size == "small") && css`
+    bottom: 1px;
+    left: 21px;
+  `}
+  }
 `;
