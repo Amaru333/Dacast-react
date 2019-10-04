f7ef9e4315c77b32a8a7c8fa6509ad8d9cbb1bc3
Quentin Benyahia <quentinbenyahia@gmail.com>
2019-10-04 12:08:39 +0100

    toggle component

---
+++ src/components/Tooltip/TooltipStyle.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { TooltipProps } from './TooltipTypes';
+
+export const StorybookInputContainerStyle = styled.div<TooltipProps  & {visibility: boolean; top: number; left:number}>`
+    background-color: ${props => props.theme.colors["gray-10"] };
+    border: 1px solid ${props => props.theme.colors["gray-7"] };
+    border-radius: ${props => props.theme.borderRadius };
+    display: inline-block;
+    visibility: ${props => props.visibility ? "visible" : "hidden"};
+    padding: 4px 8px;
+    position:absolute;
+    top: ${props => props.top }px;
+    left: ${props => props.left }px;
+    transition:none;
+    max-width: 250px;
+    box-shadow: 0px 2px 4px ${props => props.theme.colors.overlay20 };
+`;
