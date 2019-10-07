5fe9c8cda73856522aeab8188733484a6a272129
Jake Napper <jake.napper@dacast.com>
2019-10-07 12:03:45 +0100

    made the card story a bit less terrible

--- src/components/Card/CardStyle.tsx
+++ src/components/Card/CardStyle.tsx
@@ -3,6 +3,8 @@ import { ColorsApp } from '../../styled/types';
 
 export const CardStyle = styled.div`
     box-sizing: border-box;
+    padding: 16px;
+    display: inline-block;
     border: 1px solid ${props => props.theme.colors["gray-7"]};
     background-color: ${props => props.theme.colors["white"]}
 `
--- src/stories/cards.stories.tsx
+++ src/stories/cards.stories.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react'
 import { Card } from '../components/Card/Card';
-import { Text } from '../components/Typography/Text'
+import { Text } from '../components/Typography/Text';
+import styled from 'styled-components';
 
 storiesOf('Cards', module)
     .add('Cards', () => ( 
-        <React.Fragment>
+        <Container>
             <Card>
-              <Text size={14} weight="med">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum accusamus placeat pariatur ducimus ea eius aliquam corrupti laudantium officia. Laborum laboriosam, consequatur facilis rerum sunt eius dignissimos praesentium. Mollitia, facere?</Text>
+                <CardTitle>
+                    <Text size={14} weight="med">Number of Viewers</Text>
+                </CardTitle>
+               
+                <CardContent>
+                    <Text size={48} weight="reg">301</Text>
+                </CardContent>
             </Card>
-    
-        </React.Fragment>
-
+        </Container>
     ))
+
+    var Container = styled.div`
+    height: 1400px;
+    width:auto;
+    background: #EBEFF5;
+    padding: 5px;
+    padding-top: 50px;
+    text-align: center;
+`
+    var CardContent = styled.div`
+    margin: 75px 95px 20px;
+    `
+
+    var CardTitle = styled.div`
+    float:left;
+    `
+    
