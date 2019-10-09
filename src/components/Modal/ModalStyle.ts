c657f3bbe46c8df4e5f92afae294d9ebef21e388
Jake Napper <jake.napper@dacast.com>
2019-10-09 11:02:32 +0100

    added padding to modal icon for correct alignment

--- src/components/Modal/ModalStyle.ts
+++ src/components/Modal/ModalStyle.ts
@@ -5,6 +5,8 @@ import { ColorsApp } from '../../styled/types';
 export const IconStyle = styled.div<{ iconColor: ColorsApp }>`
     color: ${props => props.theme.colors[props.iconColor]};
     float: left;
+    margin-top: 4px;
+    margin-right: 8px;
 `;
 
 export const OverlayStyle = styled.div<{ opened: boolean }>`
