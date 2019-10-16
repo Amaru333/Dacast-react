c055552d5891559d44239ebf3d53b8150007feb4
Quentin Benyahia <quentinbenyahia@gmail.com>
2019-10-16 14:40:31 +0200

    Form validations ready

--- src/stories/validations.stories.tsx
+++ src/stories/validations.stories.tsx
@@ -6,8 +6,7 @@ import { Input } from '../components/FormsComponents/Input/Input';
 import { Text } from '../components/Typography/Text';
 import { Button } from '../components/FormsComponents/Button/Button';
 import { InputCheckbox } from '../components/FormsComponents/Input/InputCheckbox';
-import { formValidator, ValidationsInputObject } from '../utils/hooksFormValidator';
-import { formSubmit, ValueInput } from '../utils/hooksFormSubmit';
+import { formSubmit, ValueInput, ValidationsInputObject, handleValidationProps } from '../utils/hooksFormSubmit';
 import { action } from '@storybook/addon-actions';
 
 const stories = storiesOf('Validations', module);
@@ -24,48 +23,41 @@ export const SignupFormExemple = (props: { action: Function }) => {
 
     let formRef = React.useRef<HTMLFormElement>(null);
 
-    const data = formValidator(formRef);
-    const dataInput = formSubmit(formRef);
+    const {value, validations, enabledSubmit} = formSubmit(formRef);
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>, data: ValueInput) => {
         event.preventDefault();
         props.action(data);
     }
 
-    //Export this from somewhere else 
-    const handleValidationProps = (id: string, data: ValidationsInputObject) => {
-        return {
-            isError: data[id] ? data[id].error : false,
-            help: data[id] ? data[id].errorMessage : ""
-        }
-    }
+    console.log(enabledSubmit);
 
     return (
         <ModalCard className="mx-auto" size="small" title="Sign Up" >
-            <form onSubmit={(event) => handleSubmit(event, dataInput)} ref={formRef} noValidate>
+            <form onSubmit={(event) => handleSubmit(event, value)} ref={formRef} noValidate>
                 <ModalContent className="clearfix">
                     <div className="col col-12">
                         <Input type="text" className="col col-6 pr1" id="firstName" label="First Name" placeholder="First Name" required
-                            {...handleValidationProps('firstName', data)}
+                            {...handleValidationProps('firstName', validations)}
                         />
                         <Input type="text" className="col col-6 pl1" id="lastName" label="Last Name" placeholder="Last Name" required
-                            {...handleValidationProps('lastName', data)}
+                            {...handleValidationProps('lastName', validations)}
 
                         />
                     </div>
-                    <Input className="col col-12" type="url" id="companyWebsite" label="Company Website" placeholder="Company Website" required
-                        {...handleValidationProps('companyWebsite', data)}
+                    <Input className="col col-12" id="companyWebsite" label="Company Website" placeholder="Company Website" required
+                        {...handleValidationProps('companyWebsite', validations)}
                     />
 
                     <Input className="col col-12" type="email" id="email" label="Email Address" placeholder="Email Address" required
-                        {...handleValidationProps('email', data)}
+                        {...handleValidationProps('email', validations)}
                     />
                     <Input className="col col-12" type="tel" id="contactNumber" label="Contact Number" placeholder="Contact Number" required
-                        {...handleValidationProps('contactNumber', data)}
+                        {...handleValidationProps('contactNumber', validations)}
 
                     />
                     <Input className="col col-12" type="password" id="password" label="Password" placeholder="Password" icon="visibility_off" required
-                        {...handleValidationProps('password', data)}
+                        {...handleValidationProps('password', validations)}
 
                     />
                     <p>
@@ -75,7 +67,7 @@ export const SignupFormExemple = (props: { action: Function }) => {
                     <InputCheckbox id="checkbox1" label="Checkbox" />
                 </ModalContent>
                 <ModalFooter>
-                    <Button disabled={/**Handle disabled in Hook */ false} type="submit" sizeButton="large" typeButton="primary">Sign Up</Button>
+                    <Button disabled={!enabledSubmit} type="submit" sizeButton="large" typeButton="primary">Sign Up</Button>
                     <Button sizeButton="large" typeButton="tertiary">Cancel</Button>
                 </ModalFooter>
             </form>
