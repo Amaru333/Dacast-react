0748e9d30fca3ccb978f0e5062319162178c76d6
Clement <clementdacast@clements-mbp.home>
2019-10-17 14:29:02 +0100

    Fixing broken build

--- src/stories/tabs.stories.tsx
+++ src/stories/tabs.stories.tsx
@@ -35,20 +35,6 @@ const functionTest4 = () => {
     )
 }
 
-const returnRouter = (props: Routes[]) => {
-    return (
-        props.map((route: Routes, i: number) => {
-            return <Route key={i}
-                path={route.path}
-                render={props => (
-                    // pass the sub-routes down to keep nesting
-                    <route.component {...props} routes={route.slug} />
-                )}
-            />
-        })
-    )
-}
-
 const routeList: Routes[] = [
     {
         name:'Route1',
@@ -92,9 +78,7 @@ storiesOf('Tabs', module)
                             history={history}
                         />
                     </StorybookTabContainerStyle>
-                    <Switch>
-                        {returnRouter(routeList)}
-                    </Switch>
+
                 </Router>
             </React.Fragment>
 
