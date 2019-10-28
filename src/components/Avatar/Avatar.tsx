8f3b1bae1c8d2c54d7ddb2c1a502ca8cf0ca9a0a
Jake Napper <jake.napper@dacast.com>
2019-10-28 10:40:25 +0000

    moved the colors array to style types and generally tidied up the component

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,65 +1,52 @@
 import * as React from "react";
 import { Text } from "../Typography/Text";
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { AvatarProps } from './AvatarTypes';
+import { AvatarColorsArray } from '../../styled/types';
 
 const getInitials = (name: string) => {
     var names = name.split(' '),
         initials = names[0].substring(0, 1).toUpperCase();
         initials += names[names.length - 1].substring(0, 1).toUpperCase();
     
-        return initials;
+    return initials;
 }
 
-var colors = [
-    "#D14642",
-    "#DE8536",
-    "#FFB75D",
-    "#1E874B",
-    "#2899F6",
-    "#7048E8",
-    "#FC427B"
-  ];
-
-  const getColor = (name: string) => {
+const getColor = (name: string) => {
     var hash = 0;
-    
+
     if (name.length === 0) return hash;
     for (var i = 0; i < name.length; i++) {
         hash = name.charCodeAt(i) + ((hash << 5) - hash);
         hash = hash & hash;
     }
-    console.log(hash % colors.length)
-    return Math.abs(hash % colors.length);
-
+    return Math.abs(hash % AvatarColorsArray.length);
 }
 
 export const Avatar = (props: AvatarProps) => {
 
     return (
         <AvatarStyle {...props}>
-            {
-                <TextStyle color="white" size={16} weight="med">{getInitials(props.name)}</TextStyle>
-            }
+            
+            <TextStyle color="white" size={20} weight="med">{getInitials(props.name)}</TextStyle>
+            
         </AvatarStyle>
     )
 }
 
 export const AvatarStyle = styled.div<AvatarProps>`
-background-color: ${props => colors[getColor(props.name)]};
-width: 40px;
-height: 40px;
-border-radius: 50%;
-text-align: center;
-position: relative;
-display: table;
+    background-color: ${props => props.theme.colors[AvatarColorsArray[getColor(props.name)]]};
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    text-align: center;
+    position: relative;
+    display: table;
 `
 
 export const TextStyle = styled(Text)`
-display: table-cell;
-vertical-align: middle;
-text-align: center;
-font-size: 20px;
-line-height:28px;
-letter-spacing: 0.05em;
+    display: table-cell;
+    vertical-align: middle;
+    text-align: center;
+    letter-spacing: 0.05em;
 `
