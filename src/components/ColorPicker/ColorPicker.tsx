ffc26fe47572bf2ae1e0c65879b1b16ab00626d4
Jake Napper <jake.napper@dacast.com>
2019-10-29 11:26:08 +0000

    added default colors array in props

--- src/components/ColorPicker/ColorPicker.tsx
+++ src/components/ColorPicker/ColorPicker.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { BlockPicker } from 'react-color';
 
 export const ColorPicker = () => {
+
     return (
-        <BlockPicker triangle="hide" />
+        <BlockPicker colors={["#D14642", "#DE8536", "#FFB75D", "#1E874B", "#2899F6", "#7048E8", "#FC427B"]} triangle="hide" />
     )
 }
