bbcb822f84d0088b3843a183970e6be09afe3623
Jake Napper <jake.napper@dacast.com>
2019-11-05 12:27:09 +0000

    updated hover and focus colours for secondary and tertiary buttons

--- src/components/FormsComponents/Button/ButtonTypes.ts
+++ src/components/FormsComponents/Button/ButtonTypes.ts
@@ -15,11 +15,11 @@ export interface ButtonCustomProps {
     colorObject?: ColorObject;
 }
 
-export interface ColorObject {hoverColor: ColorsApp; color: ColorsApp; focusColor: ColorsApp; disabledColor: ColorsApp; disabledTextColor: ColorsApp}
+export interface ColorObject {hoverColor: ColorsApp; primaryHoverColor: ColorsApp; color: ColorsApp; focusColor: ColorsApp; primaryFocusColor: ColorsApp; disabledColor: ColorsApp; disabledTextColor: ColorsApp}
 
 export const buttonArrayColor: {[key: string]: ColorObject } = {
-    "red": {hoverColor:"red40", color:"red", focusColor:"red60", disabledColor: "red60", disabledTextColor: "red"},
-    "blue": {hoverColor:"violet40", color:"violet", focusColor:"violet60", disabledColor: "violet60", disabledTextColor: "violet"},
+    "red": {primaryHoverColor:"red40", hoverColor:"red40", color:"red", primaryFocusColor:"red60", focusColor:"red60", disabledColor: "red60", disabledTextColor: "red"},
+    "blue": {primaryHoverColor:"violet40", hoverColor:"violet20", color:"violet", primaryFocusColor:"violet60", focusColor:"violet40", disabledColor: "violet60", disabledTextColor: "violet"},
 }
 
 export type ButtonProps = ButtonCustomProps & React.HTMLAttributes<HTMLButtonElement>;
