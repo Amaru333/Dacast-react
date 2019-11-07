58a537b56efdcc4c23dad0d2a43652aefad477a7
Jake Napper <jake.napper@dacast.com>
2019-11-07 14:09:42 +0000

    updated radio button types to include value prop and added it to all radio buttons on delivery and embed page

--- src/containers/DeliveryAndEmbed.tsx
+++ src/containers/DeliveryAndEmbed.tsx
@@ -23,13 +23,13 @@ export const DeliveryAndEmbed = () => {
             </div>
             <br/>
             <div>
-                <InputRadio name="delivery" label="Compatible Delivery" />
+                <InputRadio name="delivery" value="compatible-delivery" label="Compatible Delivery" />
                 <RadioText>
                     <Text size={14} weight="reg">
                         HTML5 Player via HLS (HTTP Streaming). Defaults to Flash player where necessary. Prioritizes playback compatibility over security. Ideal for ad-serving.
                     </Text>
                 </RadioText>
-                <InputRadio name="delivery" label="Secure Delivery" />
+                <InputRadio name="delivery" value="secure-delivery" label="Secure Delivery" />
                 <RadioText><Text size={14} weight="reg">
                 HTML5 Player with AES Encryption. The most secure option, all videos will play with encryption. Videos can only be delivered via the Dacast player using our iframe embed.
                 </Text></RadioText>
@@ -44,13 +44,13 @@ export const DeliveryAndEmbed = () => {
             </div>
             <br/>
             <div>
-                <InputRadio name="region-settings" label="Standard PoPs" />
+                <InputRadio name="region-settings" value="standard-pops" label="Standard PoPs" />
                 <RadioText>
                     <Text size={14} weight="reg">
                     High speed CDN with servers located across 6 different continents. Your video content streams from the the server closest to the viewer.
                     </Text>
                 </RadioText>
-                <InputRadio name="region-settings" label="Premium PoPs" />
+                <InputRadio name="region-settings" value="premium-pops" label="Premium PoPs" />
                 <RadioText><Text size={14} weight="reg">
                 Everything that is included with Standard PoPs PLUS Beijing & Shanghai servers for viewers in China. Video will be streamed via HTTP progressive delivery.
                 </Text></RadioText>
@@ -65,13 +65,13 @@ export const DeliveryAndEmbed = () => {
             </div>
             <br/>
             <div>
-                <InputRadio name="embed-settings" label="IFrame (Recommended)" />
+                <InputRadio name="embed-settings" value="iframe" label="IFrame (Recommended)" />
                 <RadioText>
                     <Text size={14} weight="reg">
                     The embed code includes all vzaar features such as security, analytics & customization. Your embedded videos dynamically update whenever you change your settings.
                     </Text>
                 </RadioText>
-                <InputRadio name="embed-settings" label="HTML5 Video" />
+                <InputRadio name="embed-settings" value="html5-video" label="HTML5 Video" />
                 <RadioText><Text size={14} weight="reg">
                 The most lightweight way to embed your video. Does not include all vzaar features & will not count video views. Only use if your CMS does not accept iframes or if you are an eBay user.
                 </Text></RadioText>
@@ -86,17 +86,17 @@ export const DeliveryAndEmbed = () => {
             </div>
             <br/>
             <div>
-                <InputRadio name="embed-protocol" label="HTTPS (Recommended)" />
+                <InputRadio name="embed-protocol" value="https" label="HTTPS (Recommended)" />
                 <RadioText>
                     <Text size={14} weight="reg">
                     Always use HTTP over SSL — this is the most secure setting and is what we'd most recommend.ally update whenever you change your settings.
                     </Text>
                 </RadioText>
-                <InputRadio name="embed-protocol" label="Relative" />
+                <InputRadio name="embed-protocol" value="relative" label="Relative" />
                 <RadioText><Text size={14} weight="reg">
                 Embeds will be protocol-relative and will use the same protocol as the page on which they're embedded.
                 </Text></RadioText>
-                <InputRadio name="embed-protocol" label="HTTP" />
+                <InputRadio name="embed-protocol" value="http" label="HTTP" />
                 <RadioText><Text size={14} weight="reg">
                 If for any reason you need your embeds to always use plain HTTP then use this option, although we recommend against it.
                 </Text></RadioText>
@@ -111,13 +111,13 @@ export const DeliveryAndEmbed = () => {
             </div>
             <br/>
             <div>
-                <InputRadio name="embed-size" label="Responsive (Recommended)" />
+                <InputRadio name="embed-size" value="responsive" label="Responsive (Recommended)" />
                 <RadioText>
                     <Text size={14} weight="reg">
                     Your embed codes will default to responding to the size of their container.
                     </Text>
                 </RadioText>
-                <InputRadio name="embed-size" label="Fixed" />
+                <InputRadio name="embed-size" value="fixed" label="Fixed" />
                 <RadioText><Text size={14} weight="reg">
                 Videos will default to a fixed width.
                 </Text></RadioText>
