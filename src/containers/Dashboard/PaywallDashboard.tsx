6a82da3994c28f2498eb716c47962ffdcaa06745
Clement <clementdacast@Clements-MacBook-Pro.local>
2019-11-19 16:13:13 +0000

    Changing error icon to info

--- src/containers/Dashboard/PaywallDashboard.tsx
+++ src/containers/Dashboard/PaywallDashboard.tsx
@@ -37,7 +37,7 @@ export const PaywallDashboard = (props: React.HTMLAttributes<HTMLDivElement> & {
                 <WidgetElement className={classItemHalfWidthContainer}>
                     <WidgetHeader className="flex">
                         <Text size={16} weight="med" color="gray-3"> Revenue </Text>
-                        <IconGray1 className="ml-auto">error_outline</IconGray1>
+                        <IconGray1 className="ml-auto">info_outline</IconGray1>
                     </WidgetHeader>
                     <div className="flex minContentDash justify-center items-center mb1">
                         <Text size={48} weight="reg" color="gray-1">${revenue}</Text>
