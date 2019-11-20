d2dc42fae9b6968108feab9cd08143fe7a69f2ad
Quentin Benyahia <quentinbenyahia@gmail.com>
2019-11-20 12:10:06 +0100

    fix tables for responsiveness and no header plus feedbask apiintegration

--- src/components/Table/TableStyle.ts
+++ src/components/Table/TableStyle.ts
@@ -4,24 +4,30 @@ import {TableProps} from './TableTypes';
 export const TableContainer = styled.table<TableProps>`
     height: auto;
     margin: auto;
-    border-color: ${props => props.theme.colors["gray-7"]};
     border-spacing: unset;
     border-collapse: collapse;
+    min-width: 600px;
+    border-color: ${props => props.theme.colors["gray-7"]};
 `;
 
 export const TableHeaderContainer = styled.thead<{}>`
+`;
+
+export const WrapperResponsiveContainer = styled.div<{}>`
+    overflow-x: auto;
+    border: 1px solid ${props => props.theme.colors["gray-8"]};
 `;
 
 export const TableHeaderRow = styled.tr<{}>`
     width: auto;
     height: 52px;
     background-color: ${props => props.theme.colors["gray-10"]};
-    border: 1px solid ${props => props.theme.colors["gray-8"]};
     padding-left: 16px;
 `;
 
 export const TableHeaderCell = styled.td<{}>`
     padding-left: 16px;
+    border-bottom: 1px solid ${props => props.theme.colors["gray-8"]};
 `;
 
 export const TableBodyContainer = styled.tbody<{}>`
@@ -31,7 +37,9 @@ export const TableBodyRow = styled.tr<{}>`
     width: auto;
     height: 48px;
     background-color: ${props => props.theme.colors["white"]};
-    border: 1px solid ${props => props.theme.colors["gray-8"]};
+    :not(:last-child) {
+        border-bottom: 1px solid ${props => props.theme.colors["gray-8"]};
+    }
     padding-left: 16px;
     &:hover {
         cursor: pointer;
