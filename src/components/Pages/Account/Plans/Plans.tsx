defdfd3f6b5c55edd75847a7e48844ebfee59c58
Clement <clementdacast@Clements-MacBook-Pro.local>
2020-01-02 19:13:11 +0000

    Mobile css adjustments for the carousel

--- src/components/Pages/Account/Plans/Plans.tsx
+++ src/components/Pages/Account/Plans/Plans.tsx
@@ -1,29 +1,24 @@
 import React from 'react';
 import { Text } from '../../../Typography/Text';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Card } from '../../../Card/Card';
 import { Icon } from '@material-ui/core';
 import { Button } from '../../../FormsComponents/Button/Button';
 import { CustomStepper } from '../../../Stepper/Stepper';
 import { PlanStepperFirstStep, PlanStepperSecondStep, PlanStepperThirdStep, PlanStepperFourthStep } from './PlanStepper';
-// import {isMobile} from 'react-device-detect';
+import {isMobile} from 'react-device-detect';
 import "react-responsive-carousel/lib/styles/carousel.css";
-
-import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
-
-
 import { Carousel } from 'react-responsive-carousel';
 
 
 export const PlansPage = () => {
-    const isMobile = true;
     const textClassName = 'py1';
     const marginBlocks = 'mx2';
     const stepList=[PlanStepperFirstStep, PlanStepperSecondStep, PlanStepperThirdStep, PlanStepperFourthStep]
 
     const [stepperPlanOpened, setStepperPlanOpened] = React.useState<boolean>(false);
     return (
-        <PlansPageContainer>
+        <PlansPageContainer isMobile={isMobile}>
             {
                 !isMobile ?
                     <AllowancesList className={marginBlocks}>
@@ -49,7 +44,7 @@ export const PlansPage = () => {
                         <PlanContainer className={marginBlocks}>
                             <Text size={16} weight='reg' color='gray-3'>Starter Plan</Text>
                             <Card>
-                                <PlanInfosContainer>
+                                <PlanInfosContainer isMobile={isMobile}>
                                     <Text className={textClassName} size={32} weight='med' color='gray-1'>$21 pm</Text>
                                     <Text className={textClassName} size={12} weight='reg' color='gray-5'>Billed Annually</Text>
                                     <Text className={textClassName} size={16} weight='reg' color='gray-1'>100 GB</Text>
@@ -63,7 +58,7 @@ export const PlansPage = () => {
                         <PlanContainer className={marginBlocks}>
                             <Text size={16} weight='reg' color='gray-3'>OTT Plan</Text>
                             <Card>
-                                <PlanInfosContainer>
+                                <PlanInfosContainer isMobile={isMobile}>
                                     <Text className={textClassName} size={32} weight='med' color='gray-1'>$199 pm</Text>
                                     <Text className={textClassName} size={12} weight='reg' color='gray-5'>Billed Annually</Text>
                                     <Text className={textClassName} size={16} weight='reg' color='gray-1'>200 GB</Text>
@@ -80,7 +75,7 @@ export const PlansPage = () => {
                         <PlanContainer className={marginBlocks}>
                             <Text size={16} weight='reg' color='gray-3'>Scale Plan</Text>
                             <Card>
-                                <PlanInfosContainer>
+                                <PlanInfosContainer isMobile={isMobile}>
                                     <Text className={textClassName} size={32} weight='med' color='gray-1'>$499 pm</Text>
                                     <Text className={textClassName} size={12} weight='reg' color='gray-5'>Billed Annually</Text>
                                     <Text className={textClassName} size={16} weight='reg' color='gray-1'>5000 GB</Text>
@@ -100,7 +95,7 @@ export const PlansPage = () => {
                         <PlanContainer className={marginBlocks}>
                             <Text size={16} weight='reg' color='gray-3'>Custom</Text>
                             <Card backgroundColor='violet10'>
-                                <PlanInfosContainer>
+                                <PlanInfosContainer isMobile={isMobile}>
                                     <Text className={textClassName} size={32} weight='med' color='gray-1'>Let's chat</Text>
                                     <>
                                         <Text className={textClassName} size={16} weight='reg' color='gray-1'>Do you want a custom plan that is right for you and your company?</Text>
@@ -115,11 +110,11 @@ export const PlansPage = () => {
                     </>
 
                     :
-                    <Carousel centerSlidePercentage={60} infiniteLoop centerMode swipeable showArrows={false} showThumbs={false} showStatus={false} >
+                    <Carousel centerSlidePercentage={55} infiniteLoop centerMode swipeable showArrows={false} showThumbs={false} showStatus={false} >
                         <PlanContainer className=''>
                             <Text size={16} weight='reg' color='gray-3'>Starter Plan</Text>
                             <Card>
-                                <PlanInfosContainer>
+                                <PlanInfosContainer isMobile={isMobile}>
                                     <Text className={textClassName} size={32} weight='med' color='gray-1'>$21 pm</Text>
                                     <Text className={textClassName} size={12} weight='reg' color='gray-5'>Billed Annually</Text>
                                     <Text className={textClassName} size={16} weight='reg' color='gray-1'>100 GB</Text>
@@ -133,7 +128,7 @@ export const PlansPage = () => {
                         <PlanContainer className=''>
                             <Text size={16} weight='reg' color='gray-3'>OTT Plan</Text>
                             <Card>
-                                <PlanInfosContainer>
+                                <PlanInfosContainer isMobile={isMobile}>
                                     <Text className={textClassName} size={32} weight='med' color='gray-1'>$199 pm</Text>
                                     <Text className={textClassName} size={12} weight='reg' color='gray-5'>Billed Annually</Text>
                                     <Text className={textClassName} size={16} weight='reg' color='gray-1'>200 GB</Text>
@@ -150,7 +145,7 @@ export const PlansPage = () => {
                         <PlanContainer className=''>
                             <Text size={16} weight='reg' color='gray-3'>Scale Plan</Text>
                             <Card>
-                                <PlanInfosContainer>
+                                <PlanInfosContainer isMobile={isMobile}>
                                     <Text className={textClassName} size={32} weight='med' color='gray-1'>$499 pm</Text>
                                     <Text className={textClassName} size={12} weight='reg' color='gray-5'>Billed Annually</Text>
                                     <Text className={textClassName} size={16} weight='reg' color='gray-1'>5000 GB</Text>
@@ -170,7 +165,7 @@ export const PlansPage = () => {
                         <PlanContainer className=''>
                             <Text size={16} weight='reg' color='gray-3'>Custom</Text>
                             <Card backgroundColor='violet10'>
-                                <PlanInfosContainer>
+                                <PlanInfosContainer isMobile={isMobile}>
                                     <Text className={textClassName} size={32} weight='med' color='gray-1'>Let's chat</Text>
                                     <>
                                         <Text className={textClassName} size={16} weight='reg' color='gray-1'>Do you want a custom plan that is right for you and your company?</Text>
@@ -183,9 +178,6 @@ export const PlansPage = () => {
                             </Card>
                         </PlanContainer>
                     </Carousel>
-
-
-
             }
 
          
@@ -205,8 +197,10 @@ export const PlansPage = () => {
     )
 }
 
-const PlansPageContainer = styled.div`
-
+const PlansPageContainer = styled.div<{isMobile: boolean}>`
+    ${props => !props.isMobile && css`
+    display: flex;
+    `}
 `
 const AllowancesList = styled.div`
     display: flex;
@@ -218,15 +212,19 @@ const PlanContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    max-width: 350px;
 `
 
-const PlanInfosContainer = styled.div`
+const PlanInfosContainer = styled.div<{isMobile: boolean}>`
     display: flex;
     flex-direction: column;
     position: relative;
     align-items: center;
     height: 90%;
     min-height: 750px;
+    ${props => props.isMobile && css`
+        min-height: 600px;
+    `}
 `
 
 const IconStyle = styled(Icon)`
@@ -236,4 +234,5 @@ const IconStyle = styled(Icon)`
 const ButtonStyle = styled(Button)`
     position: absolute;
     bottom: 0;
+    width: 95%;
 `
