7d393aa535da812f9fe978f38bf1412758882b7b
Quentin Benyahia <quentinbenyahia@gmail.com>
2020-01-15 16:23:30 +0100

    filtering

--- src/pages/Videos/VideosList/VideosList.tsx
+++ src/pages/Videos/VideosList/VideosList.tsx
@@ -7,7 +7,12 @@ import { VodItem } from '../../../redux-flow/store/VOD/General/types';
 import { Label } from '../../../components/FormsComponents/Label/Label';
 import { InputCheckbox } from '../../../components/FormsComponents/Input/InputCheckbox';
 import styled from "styled-components";
-import {VideoTabs} from '../../../containers/Videos/VideoTabs';
+import { VideoTabs } from '../../../containers/Videos/VideoTabs';
+import { VideosFiltering } from './VideosFiltering';
+import { Button } from '../../../components/FormsComponents/Button/Button';
+import { DateSinglePicker } from '../../../components/FormsComponents/Datepicker/DateSinglePicker';
+import { InputSlider } from '../../../components/FormsComponents/Input/InputSlider';
+import { Badge } from '../../../components/Badge/Badge';
 
 export interface VideosListProps {
     items: VodItem[];
@@ -21,22 +26,73 @@ export const VideosListPage = (props: VideosListProps) => {
     const [showVodTabs, setShowVodTabs] = React.useState<boolean>(false);
     const [selectedVodId, setSelectedVodId] = React.useState<number>(-1);
 
-    React.useEffect(() => {
 
-    }, [selectedVod])
+
+    interface FilteringState {
+        status: {
+            online: boolean;
+            offline: boolean;
+            processing: boolean;
+        };
+        features: {
+            paywall: boolean;
+            advertising: boolean;
+            playlists: boolean;
+            rewind: boolean;
+            recording: boolean;
+        };
+        afterDate: number | boolean;
+        beforedate: number | boolean;
+        sizeStart: number | boolean;
+        sizeEnd: number | boolean;
+    }
+
+    var filteringDefault = {
+        status: {
+            online: false,
+            offline: false,
+            processing: false
+        },
+        features: {
+            paywall: false,
+            advertising: false,
+            playlists: false,
+            rewind: false,
+            recording: false
+        },
+        afterDate: false,
+        beforedate: false,
+        sizeStart: false,
+        sizeEnd: false
+    }
+    const [filteringState, setFilteringState] = React.useState<FilteringState>(filteringDefault);
+    const [activeFilter, setActiveFilter] = React.useState<number>(0);
+
+    const checkActiveFilter = () => {
+        var counter = 0;
+        Object.entries(filteringState.features).map(item => item[1] !== false ? counter++ : null);
+        Object.entries(filteringState.status).map(item => item[1] !== false ? counter++ : null)
+        filteringState.afterDate ? counter++ : null;
+        filteringState.beforedate ? counter++ : null;
+        filteringState.sizeStart ? counter++ : null;
+        filteringState.sizeEnd ? counter++ : null;
+        setActiveFilter(counter);
+    }
+    React.useEffect(() => {
+        checkActiveFilter();
+    }, [selectedVod, filteringState])
 
     const vodListHeaderElement = () => {
         return [
-            <InputCheckbox className="inline-flex" label="" key="checkboxVodListBulkAction" indeterminate={selectedVod.length >= 1 && selectedVod.length < props.items.length} defaultChecked={selectedVod.length === props.items.length} id="globalCheckboxVodList" onChange={(event) => {
-                console.log(event.currentTarget.checked, "oco1");
-                if (event.currentTarget.checked) {
-                    const editedSelectedVod = props.items.map(item => { return item.id })
-                    setSelectedVod(editedSelectedVod);
-                } else if (event.currentTarget.indeterminate || !event.currentTarget.checked) {
-                    setSelectedVod([])
-                }
-            }
-            } />,
+            <InputCheckbox className="inline-flex" label="" key="checkboxVodListBulkAction" indeterminate={selectedVod.length >= 1 && selectedVod.length < props.items.length} defaultChecked={selectedVod.length === props.items.length} id="globalCheckboxVodList"
+                onChange={(event) => {
+                    if (event.currentTarget.checked) {
+                        const editedSelectedVod = props.items.map(item => { return item.id })
+                        setSelectedVod(editedSelectedVod);
+                    } else if (event.currentTarget.indeterminate || !event.currentTarget.checked) {
+                        setSelectedVod([])
+                    }
+                }} />,
             <></>,
             <Text key="nameVodList" size={14} weight="med" color="gray-1">Name</Text>,
             <Text key="sizeVodList" size={14} weight="med" color="gray-1">Size</Text>,
@@ -44,7 +100,7 @@ export const VideosListPage = (props: VideosListProps) => {
             <Text key="viewsVodList" size={14} weight="med" color="gray-1">Created</Text>,
             <Text key="statusVodList" size={14} weight="med" color="gray-1">Status</Text>,
             <Text key="statusVodList" size={14} weight="med" color="gray-1">Features</Text>,
-            <div style={{width: "80px"}} ></div>,
+            <div style={{ width: "80px" }} ></div>,
         ]
     }
 
@@ -66,7 +122,7 @@ export const VideosListPage = (props: VideosListProps) => {
         if (props.items) {
             return props.items.map((value) => {
                 return [
-                    <InputCheckbox className="inline-flex" label="" key={"checkbox" + value.id} defaultChecked={selectedVod.includes(value.id)} id={"checkbox" + value.id.toString()} onChange={(event) => {
+                    <InputCheckbox className="inline-flex" label="" key={"checkbox" + value.id} defaultChecked={selectedVod.includes(value.id)} id={"checkboxVod" + value.id.toString()} onChange={(event) => {
                         if (event.currentTarget.checked && selectedVod.length < props.items.length) {
                             setSelectedVod([...selectedVod, value.id])
                         } else {
@@ -82,17 +138,80 @@ export const VideosListPage = (props: VideosListProps) => {
                     <Text key={"created" + value.id} size={14} weight="reg" color="gray-1">{tsToLocaleDate(value.created)}</Text>,
                     <Text key={"status" + value.id} size={14} weight="reg" color="gray-1">{value.online ? <Label backgroundColor="green20" color="green" label="Online" /> : <Label backgroundColor="red20" color="red" label="Offline" />}</Text>,
                     <>{handleFeatures(value)}</>,
-                    <div key={"more" + value.id} className="iconAction right mr2" ><Icon onClick={() => {setSelectedVodId(value.id);setShowVodTabs(true)} } className="right mr1" >edit</Icon><Icon onClick={() => { props.deleteVodList(value.title) }} className="right mr1" >delete</Icon></div>,
+                    <div key={"more" + value.id} className="iconAction right mr2" ><Icon onClick={() => { setSelectedVodId(value.id); setShowVodTabs(true) }} className="right mr1" >edit</Icon><Icon onClick={() => { props.deleteVodList(value.title) }} className="right mr1" >delete</Icon></div>,
                 ]
             })
         }
     }
 
+    const [openFilters, setOpenFilters] = React.useState<boolean>(false);
 
     return (
         showVodTabs ?
             <VideoTabs setShowVideoTabs={setShowVodTabs} videoId={selectedVodId.toString()} history={props.history} />
-            : <Table className="col-12" id="apiKeysTable" header={vodListHeaderElement()} body={vodListBodyElement()} />
+            :
+            <>
+                <div className="mb2 clearfix">
+                    <Button buttonColor="blue" className="relative right" onClick={() => setOpenFilters(!openFilters)} sizeButton="small" typeButton="secondary" >
+                        Filter
+                        <Badge color="dark-violet" style={{ top: "-8px" }} number={activeFilter} className="absolute" />
+                    </Button>
+                </div>
+                <VideosFiltering isOpen={openFilters} >
+                    <div className="flex mb25" ><Text size={24} weight="med" color="gray-1" >Filters</Text><Icon className="ml-auto pointer" onClick={() => setOpenFilters(false)} >close</Icon></div>
+                    <div className="mb3" id="vodFilterStatus">
+                        <Text className="mb2 inline-block" size={16} weight="med" color="gray-1" >Status</Text>
+                        <InputCheckbox className="mb2" defaultChecked={filteringState.status.online}
+                            onChange={(e) => { setFilteringState(prevState => { return { ...prevState, status: { ...prevState.status, online: !prevState.status.online } } }) }}
+                            id='vodFilterOnline' label="Online" labelWeight="reg" />
+                        <InputCheckbox className="mb2" defaultChecked={filteringState.status.offline}
+                            onChange={(e) => { setFilteringState(prevState => { return { ...prevState, status: { ...prevState.status, offline: !prevState.status.offline } } }) }}
+                            id='vodFilterOffline' label="Offline" labelWeight="reg" />
+                        <InputCheckbox className="mb2" defaultChecked={filteringState.status.processing}
+                            onChange={(e) => { setFilteringState(prevState => { return { ...prevState, status: { ...prevState.status, processing: !prevState.status.processing } } }) }}
+                            id='vodFilterProcessing' label="Processing" labelWeight="reg" />
+                    </div>
+                    <div className="mb3" id="vodFilterFeatures">
+                        <Text className="mb2 inline-block" size={16} weight="med" color="gray-1" >Features</Text>
+                        <InputCheckbox className="mb2" defaultChecked={filteringState.features.paywall}
+                            onChange={(e) => { setFilteringState(prevState => { return { ...prevState, features: { ...prevState.features, paywall: !prevState.features.paywall } } }) }}
+                            id='vodFilterPaywall' label="Paywall" labelWeight="reg" />
+                        <InputCheckbox className="mb2" defaultChecked={filteringState.features.advertising}
+                            onChange={(e) => { setFilteringState(prevState => { return { ...prevState, features: { ...prevState.features, advertising: !prevState.features.advertising } } }) }}
+                            id='vodFilterAdvertising' label="Advertising" labelWeight="reg" />
+                        <InputCheckbox className="mb2" defaultChecked={filteringState.features.playlists}
+                            onChange={(e) => { setFilteringState(prevState => { return { ...prevState, features: { ...prevState.features, playlists: !prevState.features.playlists } } }) }}
+                            id='vodFilterPlaylists' label="Playlists" labelWeight="reg" />
+                        <InputCheckbox className="mb2" defaultChecked={filteringState.features.rewind}
+                            onChange={(e) => { setFilteringState(prevState => { return { ...prevState, features: { ...prevState.features, rewind: !prevState.features.rewind } } }) }}
+                            id='vodFilterRewind' label="30 Minutes Rewind" labelWeight="reg" />
+                        <InputCheckbox className="mb2" defaultChecked={filteringState.features.recording}
+                            onChange={(e) => { setFilteringState(prevState => { return { ...prevState, features: { ...prevState.features, recording: !prevState.features.recording } } }) }}
+                            id='vodFilterRecording' label="Recording" labelWeight="reg" />
+                    </div>
+                    <div className="mb3" id="vodFilterAfter">
+                        <Text className="mb2 inline-block" size={16} weight="med" color="gray-1" >Created After</Text>
+                        <DateSinglePicker callback={(date: string, ms: number) => { setFilteringState(prevState => { return { ...prevState, createdAfter: ms } }) }} />
+                    </div>
+                    <div className="mb3" id="vodFilterBefore">
+                        <Text className="mb2 inline-block" size={16} weight="med" color="gray-1" >Created Before</Text>
+                        <DateSinglePicker callback={(date: string, ms: number) => { setFilteringState(prevState => { return { ...prevState, createdBefore: ms } }) }} />
+                    </div>
+                    <div className="mb3" id="vodFilterSize">
+                        <Text className="mb2 inline-block" size={16} weight="med" color="gray-1" >Size</Text>
+                        <InputSlider min={0} max={450} value={[30, 100]} id="vodFilterSize" />
+                    </div>
+                    <div className="flex" id="vodFilterbuttons">
+                        <Button onClick={ () => { setOpenFilters(false) }} className="mr1" typeButton="primary">
+                            Apply
+                        </Button>
+                        <Button onClick={ () => { setFilteringState(filteringDefault) }} typeButton="tertiary">
+                            Reset
+                        </Button>
+                    </div>
+                </VideosFiltering>
+                <Table className="col-12" id="apiKeysTable" header={vodListHeaderElement()} body={vodListBodyElement()} />
+            </>
 
     )
 
