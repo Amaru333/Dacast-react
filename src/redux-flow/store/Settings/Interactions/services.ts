7f929cbb993bd0e32ece51753868bc03b3d09345
Clement <clementdacast@Clements-MacBook-Pro.local>
2020-01-17 14:19:29 -0800

    Changing endpoints name

--- src/redux-flow/store/Settings/Interactions/services.ts
+++ src/redux-flow/store/Settings/Interactions/services.ts
@@ -4,35 +4,35 @@ import { InteractionsInfos, Ad, MailCatcher } from './types';
 const urlBase = 'https://ca282677-31e5-4de4-8428-6801321ac051.mock.pstmn.io/';
 
 const getInteractionsInfos = () => {
-    return axios.get(urlBase + 'setting-interactions');
+    return axios.get(urlBase + 'settings-interactions');
 }
 
 const saveInteractionsInfos = (data: InteractionsInfos) => {
-    return axios.put(urlBase + 'setting-interactions', {data: data});
+    return axios.put(urlBase + 'settings-interactions', {data: data});
 }
 
 const saveAd = (data: Ad) => {
-    return axios.put(urlBase + 'setting-interactions-ad', {data: data})
+    return axios.put(urlBase + 'settings-interactions-ad', {data: data})
 }
 
 const createAd = (data: Ad) => {
-    return axios.post(urlBase + 'setting-interactions-ad', {data: data})
+    return axios.post(urlBase + 'settings-interactions-ad', {data: data})
 }
 
 const deleteAd = (data: Ad) => {
-    return axios.delete(urlBase + 'setting-interactions-ad', {data: data})
+    return axios.delete(urlBase + 'settings-interactions-ad', {data: data})
 }
 
 const saveMailCatcher = (data: MailCatcher) => {
-    return axios.put(urlBase + 'setting-interactions-mailCatcher', {data: data})
+    return axios.put(urlBase + 'settings-interactions-mail-catcher', {data: data})
 }
 
 const createMailCatcher = (data: MailCatcher) => {
-    return axios.post(urlBase + 'setting-interactions-mailCatcher', {data: data})
+    return axios.post(urlBase + 'settings-interactions-mail-catcher', {data: data})
 }
 
 const deleteMailCatcher = (data: MailCatcher) => {
-    return axios.delete(urlBase + 'setting-interactions-mailCatcher', {data: data})
+    return axios.delete(urlBase + 'settings-interactions-mail-catcher', {data: data})
 }
 
 export const interactionsServices = {
