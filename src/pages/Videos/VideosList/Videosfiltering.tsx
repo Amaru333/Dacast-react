b9f8a985ec1b2f4b60964142672639e747becd65
Quentin Benyahia <quentinbenyahia@gmail.com>
2020-01-20 17:21:54 +0100

    ok things are back

--- src/pages/Videos/VideosList/Videosfiltering.tsx
+++ src/pages/Videos/VideosList/Videosfiltering.tsx
@@ -8,39 +8,41 @@ import { Icon } from '@material-ui/core';
 import { Text } from '../../../components/Typography/Text';
 import { Input } from '../../../components/FormsComponents/Input/Input';
 
-export const LivesFiltering = (props: {}) => {
+export const VideosFiltering = (props: {}) => {
 
 
     interface FilteringState {
         status: {
             online: boolean;
             offline: boolean;
+            processing: boolean;
         };
         features: {
             paywall: boolean;
             advertising: boolean;
             playlists: boolean;
-            rewind: boolean;
-            recording: boolean;
         };
         afterDate: number | boolean;
         beforedate: number | boolean;
+        sizeStart: number | boolean;
+        sizeEnd: number | boolean;
     }
 
     var filteringDefault = {
         status: {
             online: false,
-            offline: false
+            offline: false,
+            processing: false
         },
         features: {
             paywall: false,
             advertising: false,
-            playlists: false,
-            rewind: false,
-            recording: false
+            playlists: false
         },
         afterDate: false,
-        beforedate: false
+        beforedate: false,
+        sizeStart: false,
+        sizeEnd: false
     }
 
     const [filteringState, setFilteringState] = React.useState<FilteringState>(filteringDefault);
@@ -53,6 +55,8 @@ export const LivesFiltering = (props: {}) => {
         Object.entries(filteringState.status).map(item => item[1] !== false ? counter++ : null)
         filteringState.afterDate ? counter++ : null;
         filteringState.beforedate ? counter++ : null;
+        filteringState.sizeStart ? counter++ : null;
+        filteringState.sizeEnd ? counter++ : null;
         setActiveFilter(counter);
     }
 
@@ -79,6 +83,9 @@ export const LivesFiltering = (props: {}) => {
                     <InputCheckbox className="mb2" defaultChecked={filteringState.status.offline}
                         onChange={(e) => { setFilteringState(prevState => { return { ...prevState, status: { ...prevState.status, offline: !prevState.status.offline } } }) }}
                         id='vodFilterOffline' label="Offline" labelWeight="reg" />
+                    <InputCheckbox className="mb2" defaultChecked={filteringState.status.processing}
+                        onChange={(e) => { setFilteringState(prevState => { return { ...prevState, status: { ...prevState.status, processing: !prevState.status.processing } } }) }}
+                        id='vodFilterProcessing' label="Processing" labelWeight="reg" />
                 </div>
                 <div className="mb3" id="vodFilterFeatures">
                     <Text className="mb2 inline-block" size={16} weight="med" color="gray-1" >Features</Text>
@@ -91,12 +98,6 @@ export const LivesFiltering = (props: {}) => {
                     <InputCheckbox className="mb2" defaultChecked={filteringState.features.playlists}
                         onChange={(e) => { setFilteringState(prevState => { return { ...prevState, features: { ...prevState.features, playlists: !prevState.features.playlists } } }) }}
                         id='vodFilterPlaylists' label="Playlists" labelWeight="reg" />
-                    <InputCheckbox className="mb2" defaultChecked={filteringState.features.rewind}
-                        onChange={(e) => { setFilteringState(prevState => { return { ...prevState, features: { ...prevState.features, rewind: !prevState.features.rewind } } }) }}
-                        id='vodFilterRewind' label="30 Minutes Rewind" labelWeight="reg" />
-                    <InputCheckbox className="mb2" defaultChecked={filteringState.features.recording}
-                        onChange={(e) => { setFilteringState(prevState => { return { ...prevState, features: { ...prevState.features, recording: !prevState.features.recording } } }) }}
-                        id='vodFilterRecording' label="Recording" labelWeight="reg" />
                 </div>
                 <div className="mb3" id="vodFilterAfter">
                     <Text className="mb2 inline-block" size={16} weight="med" color="gray-1" >Created After</Text>
@@ -105,6 +106,13 @@ export const LivesFiltering = (props: {}) => {
                 <div className="mb3" id="vodFilterBefore">
                     <Text className="mb2 inline-block" size={16} weight="med" color="gray-1" >Created Before</Text>
                     <DateSinglePicker callback={(date: string, ms: number) => { setFilteringState(prevState => { return { ...prevState, createdBefore: ms } }) }} />
+                </div>
+                <div className="mb3" id="vodFilterSize">
+                    <Text className="mb2 inline-block" size={16} weight="med" color="gray-1" >Size</Text>
+                    <div className="mxn2 clearfix">
+                        <Input className="col col-6 px2" label="Min (Gb)" type="number" value={filteringState.sizeStart ? filteringState.sizeStart.toString() : ""} onChange={(event) => setFilteringState(prevState => { return { ...prevState, sizeStart: parseInt(event.currentTarget.value) } })} />
+                        <Input className="col col-6 px2" label="Max (Gb)" type="number" value={filteringState.sizeEnd ? filteringState.sizeEnd.toString() : ""} onChange={(event) => setFilteringState(prevState => { return { ...prevState, sizeEnd: parseInt(event.currentTarget.value) } })} />
+                    </div>
                 </div>
                 <div className="flex" id="vodFilterbuttons">
                     <Button onClick={() => { setOpenFilters(false) }} className="mr1" typeButton="primary">
