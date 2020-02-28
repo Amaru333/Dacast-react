4e0a59081b2f63d1e1508d25cb58c478342ac44e
Clement Bissonnier <clementbissonnier@Clements-MacBook-Pro.local>
2020-02-28 10:33:08 -0800

    Datepicker responsiveness beginning

--- src/components/FormsComponents/Datepicker/DateRangePickerWrapper.tsx
+++ src/components/FormsComponents/Datepicker/DateRangePickerWrapper.tsx
@@ -3,18 +3,20 @@ import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 import { DateRangePicker } from 'react-dates';
 import './datepicker_override.css'
+import { useMedia } from '../../../utils/utils';
 
 export const DateRangePickerWrapper = () => {
     const [dates, setDates] = React.useState<{startDate: any; endDate: any}>({startDate: null, endDate: null})
     const [focusedInput, setFocusedInput] = React.useState<any>(null)
-
+    let mobile = useMedia('(max-width: 780px)')
 
     return (
         <div className='right'>
-            <DateRangePicker 
-                keepOpenOnDateSelect 
+            <DateRangePicker  
                 showDefaultInputIcon={false}
                 showClearDates
+                orientation={mobile ? 'vertical' : 'horizontal'}
+                withFullScreenPortal={mobile}
                 inputIconPosition='after'
                 startDatePlaceholderText='Select date'
                 endDatePlaceholderText='Select date'
