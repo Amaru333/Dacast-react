a75113d04c3db935f5cea24c0ebb256621da168c
Clement Bissonnier <clementbissonnier@Clements-MacBook-Pro.local>
2020-03-05 11:50:55 -0800

    OOps...

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -77,7 +77,7 @@ export const Table = (props: TableProps) => {
 
     return (
         <WrapperResponsiveContainer hasContainer={props.hasContainer}>
-            <Scrollbar className={'tableTest' + props.customClassName} style={{overflowY: 'visible'}} contentProps={{style: {position: 'relative', overflowY:'visible',  display: "inline-table"}}} scrollerProps={{style: {overflowY:'visible', position: 'relative'}}} wrapperProps={{style: {overflowY:'visible', position: 'relative'}}} removeTracksWhenNotUsed removeTrackYWhenNotUsed minimalThumbXSize={6} trackXProps={{style: {backgroundColor: 'inherit'}}} trackYProps={{style: {overflowY: 'visible'}}}>
+            <Scrollbar className={'tableTest' + (props.customClassName ? props.customClassName : '')} style={{overflowY: 'visible'}} contentProps={{style: {position: 'relative', overflowY:'visible',  display: "inline-table"}}} scrollerProps={{style: {overflowY:'visible', position: 'relative'}}} wrapperProps={{style: {overflowY:'visible', position: 'relative'}}} removeTracksWhenNotUsed removeTrackYWhenNotUsed minimalThumbXSize={6} trackXProps={{style: {backgroundColor: 'inherit'}}} trackYProps={{style: {overflowY: 'visible'}}}>
                 <TableContainer  {...props}>
             
                     {props.header ? 
