4c834449a6049d403fbae1f087da95ca0ea70b4f
Quentin Benyahia <quentinbenyahia@gmail.com>
2020-03-26 15:29:53 +0100

    forgot to put this back

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -95,7 +95,7 @@ const AppContent = () => {
 
     
         return (
-            true ?
+            isLoggedIn()  ?
                 <Route 
                     path={props.path}
                 >
