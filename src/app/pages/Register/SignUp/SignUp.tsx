c6ba27e2989ae2700369abfa8a6149767bca6e2b
Quentin Benyahia <quentinbenyahia@gmail.com>
2020-04-09 15:03:55 +0200

    A nice and easy form validations

--- src/app/pages/Register/SignUp/SignUp.tsx
+++ src/app/pages/Register/SignUp/SignUp.tsx
@@ -10,55 +10,51 @@ import { defaultStateSignup, UserInfo } from '../../../redux-flow/store/Register
 import { useHistory } from 'react-router-dom';
 import { useKeyboardSubmit } from '../../../../utils/utils';
 import { IconStyle } from '../../../../shared/Common/Icon';
+import { useForm } from 'react-hook-form'
+import { handleValidationForm } from '../../../utils/hooksFormSubmit';
 
 const logo = require('../../../../../public/assets/logo.png');
 
 
 export const SignupPage = (props: SignupContainerProps) => {
 
-    let history = useHistory()
-
-    const [userInfo, setUserInfo] = React.useState<UserInfo>(defaultStateSignup)
     const [passwordVisible, setPasswordVisible] = React.useState<boolean>(false)
+    const { register, handleSubmit, errors } = useForm({
+        reValidateMode: 'onChange',
+        mode: 'onBlur'
+    })
+    let history = useHistory();
 
-    const validateForm = () => {
-        return Object.keys(userInfo).every(key => userInfo[key] || key === 'id')
-    }
-
-    const submitSignup = () => {
-        if(validateForm) {
-            props.signup(userInfo)
+    const onSubmit = (data: UserInfo) => { 
+        props.signup(data, () => {
             history.push('/confirm-email')
-        }
+        });
     }
 
-    useKeyboardSubmit(submitSignup)
+    useKeyboardSubmit( ()=> handleSubmit(onSubmit) )
 
+    console.log(errors);
     return (<LoginContainer>
         <ImageStyle className="mx-auto" src={logo} />
         <ModalCard className="mx-auto" size="small" title="Sign Up">
             <ModalContent className="clearfix">
-                <div className="col col-12">
-                    <Input type="text" className="col col-6 pr1" label="First Name" placeholder="First Name" onChange={event => setUserInfo({...userInfo, firstName: event.currentTarget.value})} />
-                    <Input type="text" className="col col-6 pl1" label="Last Name" placeholder="Last Name" onChange={event => setUserInfo({...userInfo, lastName: event.currentTarget.value})} />
-                </div>
-                <Input className="col col-12" type="url" label="Company Website" placeholder="Company Website" onChange={event => setUserInfo({...userInfo, website: event.currentTarget.value})} />
-                <Input className="col col-12" type="email" label="Email Address" placeholder="Email Address" onChange={event => setUserInfo({...userInfo, email: event.currentTarget.value})} />
-                <Input className="col col-12" label="Phone Number" placeholder="Phone Number" onChange={event => setUserInfo({...userInfo, phone: event.currentTarget.value})} />
-                <div className='flex relative col col-12'>
-                    <Input className="col col-12" type={passwordVisible ? "text" : "password"} label="Create Password" placeholder="Password" onChange={event => setUserInfo({...userInfo, password: event.currentTarget.value})} />
-                    <IconStyle onClick={() => setPasswordVisible(!passwordVisible)} className='absolute pointer top-0 right-0 pt35 pr2' coloricon='gray-3'>{passwordVisible ? 'visibility_off' : 'visibility_on'}</IconStyle>
-                </div>
-                <p>
-                    <Text color="gray-1" size={12} weight="reg">Already have an account? <a href="/login">Log in.</a></Text><br />
-                    <div className="mt1">
-                        <Text color="gray-1" size={12} weight="reg">By signing up, you agree to the <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a></Text>
+                <form id="formSignUp" onSubmit={handleSubmit(onSubmit)}>
+                    <Input {...handleValidationForm('firstName', errors)} ref={register({ required: "Required"})} type="text" className="col col-6 pr1" label="First Name" placeholder="First Name" />
+                    <Input {...handleValidationForm('lastName', errors)} ref={register({ required: "Required" })} name="lastName" type="text" className="col col-6 pl1" label="Last Name" placeholder="Last Name" />
+                    <Input {...handleValidationForm('website', errors, 'url', register)} className="col col-12" label="Company Website" placeholder="Company Website"  />
+                    <Input {...handleValidationForm('email', errors, 'email', register)} className="col col-12" label="Email Address" placeholder="Email Address" />
+                    <Input {...handleValidationForm('phone', errors, 'tel', register)} className="col col-12" label="Phone Number" placeholder="Phone Number" />
+                    <div className='flex relative col col-12'>
+                        <Input {...handleValidationForm('password', errors, 'password', register)} className="col col-12" type={passwordVisible ? "text" : "password"} label="Create Password" placeholder="Password" />
+                        <IconStyle onClick={() => setPasswordVisible(!passwordVisible)} className='absolute pointer top-0 right-0 pt35 pr2' coloricon='gray-3'>{passwordVisible ? 'visibility_off' : 'visibility_on'}</IconStyle>
                     </div>
-
-                </p>
+                    <Text className="left" color="gray-1" size={12} weight="reg">Already have an account? <a href="/login">Log in.</a></Text><br />
+                    <Text className="left" color="gray-1" size={12} weight="reg">By signing up, you agree to the <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a></Text>
+                </form>
+                
             </ModalContent>
             <ModalFooter>
-                <Button disabled={!validateForm()} sizeButton="large" typeButton="primary" onClick={() => submitSignup()}>Sign Up</Button>
+                <Button sizeButton="large" typeButton="primary" type="submit" form="formSignUp">Sign Up</Button>
                 <Button sizeButton="large" typeButton="tertiary" onClick={() => history.push("/login")}>Cancel</Button>
             </ModalFooter>
         </ModalCard>
