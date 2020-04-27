d2313da9c41d7133e6001d0d3a630bce198c0e4d
Jake Napper <jake.napper@dacast.com>
2020-04-27 14:10:16 +0100

    pagination works correctly when number of results / displayed options isnt a whole number

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -17,6 +17,8 @@ export const Pagination = (props: PaginationProps) => {
 
     const [currentPage, setCurrentPage] = React.useState<number>(1);
     const [displayedOptions, setDisplayedOptions] = React.useState<number>(props.defaultDisplayedOption ? props.defaultDisplayedOption : props.displayedItemsOptions[0]);
+
+    const lastPage = Math.ceil(props.totalResults / displayedOptions)
     
     let smallScreen = useMedia('(max-width: 780px)')
 
@@ -26,7 +28,7 @@ export const Pagination = (props: PaginationProps) => {
 
     const renderPaginationButtons = () => {
         return Array.from(
-            {length: props.totalResults / displayedOptions < 5 ? props.totalResults / displayedOptions : 5 }, 
+            {length: props.totalResults / displayedOptions < 5 ? lastPage : 5 }, 
             (v, i) => {
                 return currentPage > 5 ? currentPage - (4 - i) : i + 1
             }).map((page) => {
@@ -53,8 +55,8 @@ export const Pagination = (props: PaginationProps) => {
                 <IconStyle coloricon={currentPage === 1 ? 'gray-7' : 'gray-3'} disabled={currentPage === 1} onClick={currentPage !== 1 ? () => {setCurrentPage(1)} : null}>first_page</IconStyle>
                 <IconStyle coloricon={currentPage === 1 ? 'gray-7' : 'gray-3'} disabled={currentPage === 1} onClick={currentPage !== 1 ? () => {setCurrentPage(currentPage - 1)} : null}>keyboard_arrow_left</IconStyle>
                 {renderPaginationButtons()}
-                <IconStyle coloricon={currentPage === props.totalResults / displayedOptions ? 'gray-7' : 'gray-3'} disabled={currentPage === props.totalResults / displayedOptions} onClick={currentPage !== props.totalResults / displayedOptions ? () => {setCurrentPage(currentPage + 1)} : null}>keyboard_arrow_right</IconStyle>
-                <IconStyle coloricon={currentPage === props.totalResults / displayedOptions ? 'gray-7' : 'gray-3'} disabled={currentPage === props.totalResults / displayedOptions} onClick={currentPage !== props.totalResults / displayedOptions ? () => {setCurrentPage(props.totalResults / displayedOptions)}: null}>last_page</IconStyle>
+                <IconStyle coloricon={currentPage === lastPage ? 'gray-7' : 'gray-3'} disabled={currentPage === lastPage} onClick={currentPage !== lastPage ? () => {setCurrentPage(currentPage + 1)} : null}>keyboard_arrow_right</IconStyle>
+                <IconStyle coloricon={currentPage === lastPage ? 'gray-7' : 'gray-3'} disabled={currentPage === lastPage} onClick={currentPage !== lastPage ? () => {setCurrentPage(lastPage)}: null}>last_page</IconStyle>
             </div>
         </div>
     )
@@ -70,4 +72,6 @@ const PaginationButton = styled.button<{currentPage: boolean}>`
     border-radius: 4px;
     align-self: flex-start;
     cursor: pointer;
+    padding: unset;
+    text-align: center;
 `
