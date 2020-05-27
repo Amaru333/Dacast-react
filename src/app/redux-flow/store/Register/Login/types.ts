072aa48970b78909cabe35410a69f4b6d2fcb2bb
Quentin Benyahia <quentinbenyahia@gmail.com>
2020-05-27 18:48:14 +0200

    iadd bubble login

--- src/app/redux-flow/store/Register/Login/types.ts
+++ src/app/redux-flow/store/Register/Login/types.ts
@@ -1,6 +1,7 @@
 export enum ActionTypes {
     LOGIN_REQUEST = '@@register_login/LOGIN_REQUEST',
     LOGIN = '@@register_login/LOGIN',
+    LOGIN_ERROR = '@@register_login/LOGIN_ERROR',
     LOGOUT = '@@register_login/LOGOUT'
 }
 
@@ -14,11 +15,13 @@ export interface TokenInfos {
     token: string;
     refresh: string;
     userId?: string;
+    error?: boolean;
 }
 
 export const defaultStateLogin: TokenInfos = {
     expires: '',
     token: '',
     refresh: '',
-    userId: ''
+    userId: '',
+    error: false
 }
