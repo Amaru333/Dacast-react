5a4159026093928589dc259e85e5d9c00d0476af
Quentin Benyahia <quentinbenyahia@gmail.com>
2020-07-08 16:12:47 +0200

    few tickets

--- src/app/shared/Billing/NewPaymentMethodForm.tsx
+++ src/app/shared/Billing/NewPaymentMethodForm.tsx
@@ -23,41 +23,40 @@ export const NewPaymentMethodForm = (props: { callback: Function; actionButton?:
 
     let formRef = React.useRef<HTMLFormElement>(null)
 
-    const {recurly} = useRecurly()
+    const { recurly } = useRecurly()
 
     React.useEffect(() => {
-        if(recurly){
+        if (recurly) {
             const paypal = recurly.PayPal('Dacast')
             setPayPalInstance(paypal)
         }
-        
     }, [])
 
     React.useEffect(() => {
-        if(payPalInstance) {
+        if (payPalInstance) {
             payPalInstance.on('token', token => {
                 console.log('Token: ', token)
-              })
-          
-              payPalInstance.on('error', error => {
+            })
+
+            payPalInstance.on('error', error => {
                 throw error
-              })
-          
-              payPalInstance.on('cancel', () => {
+            })
+
+            payPalInstance.on('cancel', () => {
                 console.log('Cancelled')
-              })
-          
-              payPalInstance.on('ready', () => {
+            })
+
+            payPalInstance.on('ready', () => {
                 console.log('Ready')
-              })
+            })
         }
 
-      }, [payPalInstance])
+    }, [payPalInstance])
 
     useStepperFinalStepAction('stepperNextButton', () => useRecurlySubmit(formRef.current, selectedOption, props.callback, recurly, props.actionButton, setThreeDSecureActionToken, setRecurlyToken, props.stepperData, payPalInstance))
 
     return (
-        <form id='paymentMethodForm' ref={formRef} onSubmit={(event) => { event.preventDefault()}} >
+        <form id='paymentMethodForm' ref={formRef} onSubmit={(event) => { event.preventDefault() }} >
             <TextStyle className='mb2'><Text size={14} weight='reg' color='gray-1'>Choose which payment method you want to use</Text></TextStyle>
             <RadioButtonContainer isSelected={selectedOption === 'creditCard'}>
                 <InputRadio name='paymentMethodForm' value='creditCard' defaultChecked={true} onChange={() => setSelectedOption('creditCard')} label='Credit Card' />
@@ -71,20 +70,20 @@ export const NewPaymentMethodForm = (props: { callback: Function; actionButton?:
                         label="Account's Holder First Name"
                         type='text'
                         required={false}
-                        onChange={(event) => props.callback({...props.billingInfo, creditCard: {...props.billingInfo.creditCard, firstName: event.currentTarget.value}})}
+                        onChange={(event) => props.callback({ ...props.billingInfo, creditCard: { ...props.billingInfo.creditCard, firstName: event.currentTarget.value } })}
                     />
                     <Input
                         data-recurly="last_name"
-                        className={ClassHalfXsFullMd+ 'pl1 mb2'}
+                        className={ClassHalfXsFullMd + 'pl1 mb2'}
                         label="Account's Holder Last Name"
                         type='text'
                         required={false}
-                        onChange={(event) => props.callback({...props.billingInfo, creditCard: {...props.billingInfo.creditCard, lastName: event.currentTarget.value}})}
+                        onChange={(event) => props.callback({ ...props.billingInfo, creditCard: { ...props.billingInfo.creditCard, lastName: event.currentTarget.value } })}
                     />
-                    <div className={ClassHalfXsFullMd+ 'pr1 mb2'}>
+                    <div className={ClassHalfXsFullMd + 'pr1 mb2'}>
                         <Text size={14} weight="med">Number</Text>
                         <RecurlyElementStyle>
-                            <CardNumberElement style={{ fontColor: Theme.colors["gray-1"], fontFamily: 'Roboto', fontSize: '14px' }} onChange={(state) => props.callback({...props.billingInfo, creditCard: {...props.billingInfo.creditCard, cardNumber: state.lastFour}})} />
+                            <CardNumberElement style={{ fontColor: Theme.colors["gray-1"], fontFamily: 'Roboto', fontSize: '14px' }} onChange={(state) => props.callback({ ...props.billingInfo, creditCard: { ...props.billingInfo.creditCard, cardNumber: state.lastFour } })} />
                         </RecurlyElementStyle>
 
                     </div>
@@ -114,36 +113,36 @@ export const NewPaymentMethodForm = (props: { callback: Function; actionButton?:
                     </div>
                     <Input
                         data-recurly="vat_number"
-                        className={ClassHalfXsFullMd+'pr1 mb2'}
+                        className={ClassHalfXsFullMd + 'pr1 mb2'}
                         label="VAT Number"
                         type='text'
                         required={false}
-                        onChange={(event) => props.callback({...props.billingInfo, creditCard: {...props.billingInfo.creditCard, vatNumber: event.currentTarget.value}})}
+                        onChange={(event) => props.callback({ ...props.billingInfo, creditCard: { ...props.billingInfo.creditCard, vatNumber: event.currentTarget.value } })}
                     />
                     <Input
                         data-recurly="country"
-                        className={ClassHalfXsFullMd+'pl1 mb2'}
+                        className={ClassHalfXsFullMd + 'pl1 mb2'}
                         label="Country"
                         type='text'
                         required={false}
-                        onChange={(event) => props.callback({...props.billingInfo, creditCard: {...props.billingInfo.creditCard, country: event.currentTarget.value}})}
+                        onChange={(event) => props.callback({ ...props.billingInfo, creditCard: { ...props.billingInfo.creditCard, country: event.currentTarget.value } })}
                     />
                     <Input
                         data-recurly="address1"
-                        className={ClassHalfXsFullMd+'pr1 mb2'}
+                        className={ClassHalfXsFullMd + 'pr1 mb2'}
                         label="Street Address 1"
                         type='text'
                         required={false}
-                        onChange={(event) => props.callback({...props.billingInfo, creditCard: {...props.billingInfo.creditCard, address1: event.currentTarget.value}})}
+                        onChange={(event) => props.callback({ ...props.billingInfo, creditCard: { ...props.billingInfo.creditCard, address1: event.currentTarget.value } })}
                     />
                     <Input
                         data-recurly="address2"
-                        className={ClassHalfXsFullMd+'pl1 mb2'}
+                        className={ClassHalfXsFullMd + 'pl1 mb2'}
                         label="Street Address 2"
                         indicationLabel='Optional'
                         type='text'
                         required={false}
-                        onChange={(event) => props.callback({...props.billingInfo, creditCard: {...props.billingInfo.creditCard, address2: event.currentTarget.value}})}
+                        onChange={(event) => props.callback({ ...props.billingInfo, creditCard: { ...props.billingInfo.creditCard, address2: event.currentTarget.value } })}
                     />
                     <Input
                         data-recurly="city"
@@ -151,7 +150,7 @@ export const NewPaymentMethodForm = (props: { callback: Function; actionButton?:
                         label="Town/City"
                         type='text'
                         required={false}
-                        onChange={(event) => props.callback({...props.billingInfo, creditCard: {...props.billingInfo.creditCard, city: event.currentTarget.value}})}
+                        onChange={(event) => props.callback({ ...props.billingInfo, creditCard: { ...props.billingInfo.creditCard, city: event.currentTarget.value } })}
                     />
                     <Input
                         data-recurly="state"
@@ -160,7 +159,7 @@ export const NewPaymentMethodForm = (props: { callback: Function; actionButton?:
                         type='text'
                         indicationLabel='Optional'
                         required={false}
-                        onChange={(event) => props.callback({...props.billingInfo, creditCard: {...props.billingInfo.creditCard, state: event.currentTarget.value}})}
+                        onChange={(event) => props.callback({ ...props.billingInfo, creditCard: { ...props.billingInfo.creditCard, state: event.currentTarget.value } })}
                     />
                     <Input
                         data-recurly="postal_code"
@@ -168,7 +167,7 @@ export const NewPaymentMethodForm = (props: { callback: Function; actionButton?:
                         label="Zip/Postal Code"
                         type='text'
                         required={false}
-                        onChange={(event) => props.callback({...props.billingInfo, creditCard: {...props.billingInfo.creditCard, postCode: event.currentTarget.value}})}
+                        onChange={(event) => props.callback({ ...props.billingInfo, creditCard: { ...props.billingInfo.creditCard, postCode: event.currentTarget.value } })}
                     />
                 </div>
                 <input type="hidden" name="recurly-token" data-recurly="token"></input>
@@ -183,20 +182,20 @@ export const NewPaymentMethodForm = (props: { callback: Function; actionButton?:
                     <Text size={14} weight='reg' color='gray-1'>
                         When you click next, you will be redirected to another website where you may securely enter your banking details. After completing the requested information you will be redirected back to Dacast.
                     </Text>
-                    <Button onClick={() => {payPalInstance.start()}}>Recurly Checkout</Button>
+                    <Button onClick={() => { payPalInstance.start() }}>Recurly Checkout</Button>
                 </div>
 
             </RadioButtonOption>
-            { threeDSecureActionToken &&
-            <div >
-                <ThreeDSecure
-                    style={{height:400}}
-                    actionTokenId={threeDSecureActionToken}
-                    onToken={(actionToken) => {props.actionButton(recurlyToken, actionToken.id)}}
-                    onError={(error) => props.handleThreeDSecureFail()}
-                >
-                </ThreeDSecure>
-            </div>
+            {threeDSecureActionToken &&
+                <div >
+                    <ThreeDSecure
+                        style={{ height: 400 }}
+                        actionTokenId={threeDSecureActionToken}
+                        onToken={(actionToken) => { props.actionButton(recurlyToken, actionToken.id) }}
+                        onError={(error) => props.handleThreeDSecureFail()}
+                    >
+                    </ThreeDSecure>
+                </div>
             }
         </form>
     )
