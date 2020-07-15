761fec68c2bcf57a381b3e6ce593d2ce65bb4da6
Clement Bissonnier <clementbissonnier@clements-mbp.lan>
2020-07-15 16:37:38 -0700

    Forgot to handle the timezone changes

--- src/app/pages/Live/General/General.tsx
+++ src/app/pages/Live/General/General.tsx
@@ -215,8 +215,9 @@ export const LiveGeneralPage = (props: LiveGeneralProps) => {
                                             className="col col-12 sm-col-4 pl1 "
                                             hasSearch
                                             dropdownTitle='Timezone'
-                                            defaultValue={props.liveDetails.countdown.timezone}
+                                            defaultValue={startDateTimeValue.timezone}
                                             id='dropdownTimezone'
+                                            callback={(value: string) => {setStartDateTimeValue({...startDateTimeValue, timezone: value.split(' ')[0]})}} 
                                             list={momentTZ.tz.names().reduce((reduced: DropdownListType, item: string) => { return { ...reduced, [item + ' (' + momentTZ.tz(item).format('Z z') + ')']: false } }, {})}
                                         />
                                     </div>
