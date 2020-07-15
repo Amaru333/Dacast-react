c9d3f0923ac0dca8218665d94272c84b713c16ab
Clement Bissonnier <clementbissonnier@clements-mbp.lan>
2020-07-15 16:06:23 -0700

    Impersonate fix token

--- src/app/pages/Impersonate/Impersonate.tsx
+++ src/app/pages/Impersonate/Impersonate.tsx
@@ -10,7 +10,14 @@ export const Impersonate = () => {
     React.useEffect(() => {
         if(query.get('token')) {
             localStorage.removeItem('userToken')
-            localStorage.setItem('userToken', JSON.stringify(query.get('token')))
+            localStorage.setItem('userToken', JSON.stringify(
+                {
+                    token: query.get('token'),
+                    accessToken: null,
+                    refresh: null,
+                    expires: 9999999999999
+                }
+            ))
             history.push('/dashboard')
         }
     }, [])
