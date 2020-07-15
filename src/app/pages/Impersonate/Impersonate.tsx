6f13547bd8944d01156813e3c4c89e05554b97cc
Clement Bissonnier <clementbissonnier@clements-mbp.lan>
2020-07-15 10:47:13 -0700

    impersonate redirection

--- src/app/pages/Impersonate/Impersonate.tsx
+++ src/app/pages/Impersonate/Impersonate.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { useQuery } from '../../../utils/utils'
+import { useHistory } from 'react-router'
 
 export const Impersonate = () => {
 
     let query = useQuery()
-
+    let history = useHistory()
 
     React.useEffect(() => {
         if(query.get('token')) {
             localStorage.removeItem('userToken')
             localStorage.setItem('userToken', JSON.stringify(query.get('token')))
+            history.push('/dashboard')
         }
     }, [])
-    
+
     return (
         <div>
             <h1>Impersonate page</h1>
