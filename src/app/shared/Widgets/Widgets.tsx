f09cf091d22a673b4913f8f5b1f8064e5ba59964
Quentin Benyahia <quentinbenyahia@gmail.com>
2020-07-20 11:02:30 +0200

    Setup playlist stuff

--- src/app/shared/Widgets/Widgets.tsx
+++ src/app/shared/Widgets/Widgets.tsx
@@ -3,13 +3,14 @@ import { Button } from '../../../components/FormsComponents/Button/Button';
 import { Text } from '../../../components/Typography/Text';
 
 export const handleButtonToPurchase = (percentage: number, purchaseItem: string, handlePurchaseStepper: Function) => {
+    console.log(purchaseItem);
     if(percentage <= 25 ) {
         return (
-            <Text className="ml-auto" size={12} weight="med" color="dark-violet"> <Button buttonColor="red" sizeButton="xs" onClick={() => handlePurchaseStepper(purchaseItem)}>Buy More</Button></Text>
+            <Text className="ml-auto" size={12} weight="med" color="dark-violet"> <Button buttonColor="red" sizeButton="xs" onClick={() => handlePurchaseStepper(purchaseItem)}>Upgrade</Button></Text>
         )
     } else {
         return (
-            <Text className="ml-auto" size={12} weight="med" color="dark-violet"> <Button typeButton="tertiary" sizeButton="xs" onClick={() => handlePurchaseStepper(purchaseItem)}>Buy More</Button></Text>
+            <Text className="ml-auto" size={12} weight="med" color="dark-violet"> <Button typeButton="tertiary" sizeButton="xs" onClick={() => handlePurchaseStepper(purchaseItem)}>Upgrade</Button></Text>
         )
     }
 }
