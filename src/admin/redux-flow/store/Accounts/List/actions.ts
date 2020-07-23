5f502fd587496aa38f3fc014fb9b126c535ffcda
Clement Bissonnier <clementbissonnier@Clements-MBP.wp.comcast.net>
2020-07-23 14:58:17 -0700

    Adding script for help section

--- src/admin/redux-flow/store/Accounts/List/actions.ts
+++ src/admin/redux-flow/store/Accounts/List/actions.ts
@@ -12,11 +12,31 @@ export interface GetAccounts {
     };
 }
 
+export interface Impersonate {
+    type: ActionTypes.IMPERSONATE,
+    payload: {
+        data: {
+            token: string
+        }
+    }
+}
+
 export const getAccountsAction = (accountId: string): ThunkDispatch<Promise<void>, {}, GetAccounts> => {
     return async (dispatch: ThunkDispatch<AdminState, {}, GetAccounts>) => {
         await AccountsServices.getAccounts(accountId)
             .then( response => {
                 dispatch({type: ActionTypes.GET_ACCOUNTS, payload: response.data});
+            }).catch(() => {
+
+            })
+    }
+}
+
+export const impersonateAction = (accountId: string): ThunkDispatch<Promise<void>, {}, Impersonate> => {
+    return async (dispatch: ThunkDispatch<AdminState, {}, Impersonate>) => {
+        await AccountsServices.impersonate(accountId)
+            .then( response => {
+                dispatch({type: ActionTypes.IMPERSONATE, payload: response.data});
             }).catch(() => {
 
             })
--- src/admin/redux-flow/store/Withdrawals/List/services.ts
+++ src/admin/redux-flow/store/Withdrawals/List/services.ts
@@ -1,9 +1,18 @@
 import axios from 'axios'
+import { isTokenExpired, addTokenToHeader } from '../../../../utils/token';
 
-const adminApiUrlBase = 'https://ca282677-31e5-4de4-8428-6801321ac051.mock.pstmn.io/'
+const adminApiUrlBase = 'https://singularity-api-admin.dacast.com/'
 
-const getWithdrawals = (accountId: string) => {  
-    return axios.get(adminApiUrlBase   + 'admin/' + (accountId ? ('withdrawals?accountId=' + accountId) : 'withdrawals'))
+const getWithdrawals = async (accountId: string) => {  
+    await isTokenExpired()
+    let {token} = addTokenToHeader();
+    return await axios.get(adminApiUrlBase + (accountId ? ('paywall?accountId=' + accountId) : 'paywall'), 
+    {
+        headers: {
+            Authorization: token
+        }
+    }
+    )
 }
 
 export const WithdrawalsServices = {
