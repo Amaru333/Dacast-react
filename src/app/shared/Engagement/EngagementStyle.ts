b0c960e43c98df4d92f7b364fc28808310427861
Jake Napper <jake.napper@dacast.com>
2020-08-03 12:08:54 +0100

    ad url in tables have max width and ellipsis for overflow

--- src/app/shared/Engagement/EngagementStyle.ts
+++ src/app/shared/Engagement/EngagementStyle.ts
@@ -18,3 +18,10 @@ export const Header = styled.div`
 display: flex;
 align-items: center;
 `
+
+export const AdTableURLContainer = styled.div`
+    max-width: 400px;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+`
