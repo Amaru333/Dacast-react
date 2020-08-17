1c9dc9145efc76ed7cc6508ba24d20244bb5f7b2
Clement Bissonnier <clementbissonnier@Clements-MBP.wp.comcast.net>
2020-08-17 13:31:17 -0700

    Forgot those

--- src/app/shared/Common/Features.tsx
+++ src/app/shared/Common/Features.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { FolderAsset } from '../../redux-flow/store/Folders/types'
 import { IconStyle, IconGreyContainer } from '../../../shared/Common/Icon'
 import { Tooltip } from '../../../components/Tooltip/Tooltip'
-import { LiveItem } from '../../redux-flow/store/Live/General/types'
+import { ContentItem } from '../../redux-flow/store/Content/General/types'
 import { PlaylistItem } from '../../redux-flow/store/Playlists/List/types'
 import { VodItem } from '../../redux-flow/store/VOD/General/types'
 import { Text } from '../../../components/Typography/Text'
@@ -16,7 +16,7 @@ export interface FeaturesList {
     folder?: boolean;
 }
 
-export const handleFeatures = (item: FolderAsset | VodItem | LiveItem | PlaylistItem, id: string): JSX.Element[] => {
+export const handleFeatures = (item: FolderAsset | VodItem | ContentItem | PlaylistItem, id: string): JSX.Element[] => {
     var element = []
     if (item.featuresList.paywall) {
         element.push(
