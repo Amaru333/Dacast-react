a630ecd809f034e14b1a218406d9c53e5f87d761
Jake Napper <jake.napper@dacast.com>
2020-08-18 14:16:56 +0100

    updated trial plan dashboard tutorial and profile completion widgets

--- src/app/containers/Dashboard/TrialAdditionalDashboard.tsx
+++ src/app/containers/Dashboard/TrialAdditionalDashboard.tsx
@@ -8,16 +8,19 @@ import { Card } from '../../../components/Card/Card';
 import { Button } from '../../../components/FormsComponents/Button/Button';
 import {useHistory} from 'react-router-dom'
 import styled from 'styled-components';
+import { AddStreamModal } from '../Navigation/AddStreamModal';
+import { userToken } from '../../utils/token';
 const faqIcon = require('../../../../public/assets/support-faq.png');
 const supportIcon = require('../../../../public/assets/support-widget.png');
 
-interface ItemTodo { isChecked: boolean; name: string; href: string }
+interface ItemTodo { isChecked: boolean; name: string; href: string, isProfile?: boolean }
 
 
 export const TrialAdditionalDashboard = (props: React.HTMLAttributes<HTMLDivElement>) => {
 
     const [supportWidgetOpen, setSupportWidgetOpen] = React.useState<boolean>(true)
     const [trialFAQWidgetOpen, setTrialFAQWidgetOpen] = React.useState<boolean>(true)
+    const [addStreamModalOpen, setAddStreamModalOpen] = React.useState<boolean>(false)
 
     let history = useHistory()
 
@@ -25,22 +28,26 @@ export const TrialAdditionalDashboard = (props: React.HTMLAttributes<HTMLDivElem
         {
             isChecked: false,
             name: "Create video",
-            href: "https://dacast.com"
+            href: "https://dacast.com",
+            isProfile: true
         },
         {
             isChecked: false,
             name: "Provide your personal details",
-            href: "https://dacast.com"
+            href: "/account/profile",
+            isProfile: true
         },
         {
             isChecked: false,
             name: "Provide your company details",
-            href: "https://dacast.com"
+            href: "/account/company",
+            isProfile: true
         },
         {
             isChecked: true,
             name: "Sign up to Dacast",
-            href: "https://dacast.com"
+            href: "https://dacast.com",
+            isProfile: true
         },
     ]
 
@@ -48,40 +55,49 @@ export const TrialAdditionalDashboard = (props: React.HTMLAttributes<HTMLDivElem
         {
             isChecked: false,
             name: "How to start streaming with OBS",
-            href: "https://dacast.com"
+            href: "https://www.dacast.com/support/knowledgebase/quick-start-with-live-streaming-obs-studio/"
 
         },
         {
             isChecked: false,
             name: "How to start streaming with Wirecast",
-            href: "https://dacast.com"
+            href: "https://www.dacast.com/support/knowledgebase/how-to-live-stream-with-wirecast/"
 
         },
         {
             isChecked: false,
             name: "How to upload a Video",
-            href: "https://dacast.com"
+            href: "https://www.dacast.com/support/knowledgebase/video-guide-how-to-upload-videos-on-pc-mac/"
 
         },
         {
             isChecked: false,
             name: "How to embed on your website",
-            href: "https://dacast.com"
+            href: "https://www.dacast.com/support/knowledgebase/how-can-i-embed-a-video-on-my-website/"
 
         },
         {
             isChecked: false,
             name: "How to customize the player theme",
-            href: "https://dacast.com"
+            href: "https://www.dacast.com/support/knowledgebase/"
 
         },
         {
             isChecked: false,
             name: "How to add security to your video: password protection, geo and referencing",
-            href: "https://dacast.com"
+            href: "https://www.dacast.com/support/knowledgebase/"
 
         }
     ]
+
+    const handleCreateStream = () => {
+        if (!userToken.getPrivilege('privilege-china') && !userToken.getPrivilege('privilege-unsecure-m3u8') && !userToken.getPrivilege('privilege-dvr') ) {
+            history.push("/livestreams")
+        } else {
+            setAddStreamModalOpen(true)
+        }
+    }
+
     return (
         <section className="col lg-col-12 sm-col-12">
             <div className={classContainer}>
@@ -89,7 +105,7 @@ export const TrialAdditionalDashboard = (props: React.HTMLAttributes<HTMLDivElem
                 <WidgetElement className={classItemHalfWidthContainer}>
                     <WidgetHeader style={{justifyContent: 'space-between'}} className="flex">
                         <Text size={16} weight="med" color="gray-1"> Tutorials </Text>
-                        <Button onClick={() => location.href='https://www.dacast.com/support/knowledgebase/'} className="col col-2" sizeButton="xs" typeButton="secondary">See More</Button>
+                        <Button onClick={() => window.open('https://www.dacast.com/support/knowledgebase/', '_blank')} className="col col-2" sizeButton="xs" typeButton="secondary">See More</Button>
                     </WidgetHeader>
                     <div className="flex mb1">
                         <TodoList items={todoItems} />
@@ -106,8 +122,8 @@ export const TrialAdditionalDashboard = (props: React.HTMLAttributes<HTMLDivElem
                             <TodoList items={todoProfileItems} />
                         </div>
                     </Card>
-                    <Button className="col-12 mb1" sizeButton="large" typeButton='secondary'> Upload a Video </Button>
-                    <Button className="col-12" sizeButton="large" typeButton='secondary'> Create a Live Stream </Button>
+                    <Button className="col-12 mb1" sizeButton="large" typeButton='secondary' onClick={() => history.push('/uploader')}> Upload a Video </Button>
+                    <Button className="col-12" sizeButton="large" typeButton='secondary' onClick={() => handleCreateStream()}> Create a Live Stream </Button>
                 </div>
                 { supportWidgetOpen &&
                     <div className={classItemFullWidthContainer}>
@@ -142,6 +158,7 @@ export const TrialAdditionalDashboard = (props: React.HTMLAttributes<HTMLDivElem
                 
                 
             </div>
+            <AddStreamModal toggle={() => setAddStreamModalOpen(false)} opened={addStreamModalOpen === true} />
         </section>
     )
 }
@@ -152,7 +169,7 @@ const TodoList = (props: { items: ItemTodo[] }) => {
         return props.items.map((value, key) => {
             return (
                 <ListItem key={key+"_"+value} checked={value.isChecked} >
-                    <LinkTag target="_blank" rel="noopener noreferrer" href={value.href} >
+                    <LinkTag target={!value.isProfile && "_blank"} rel="noopener noreferrer" href={value.href} >
                         <IconStyle className='px1 py2' coloricon={value.isChecked ? 'violet' : 'gray-8'}>check</IconStyle>
                         <Text className='line-through hover-under' color={value.isChecked ? "gray-6" : "gray-1"} size={14} weight="reg" >{value.name}</Text>
                     </LinkTag>
