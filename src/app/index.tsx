34950d890def53c30bd013141c15b4abf4ddb079
Clement Bissonnier <clementbissonnier@Clements-MBP.wp.comcast.net>
2020-09-11 19:52:19 -0700

    Fixing impersonate staying on previous user

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,6 +7,7 @@ import configureStore from "./redux-flow/configureStore";
 import { globalDefaultState } from "./redux-flow/store";
 import "react-table/react-table.css";
 import * as Sentry from '@sentry/react';
+import { userToken } from './utils/token';
 
 
 const initialState = globalDefaultState;
@@ -14,4 +15,17 @@ export const store = configureStore(initialState);
 
 Sentry.init({dsn: "https://2e329011118c44b5a76d3670883a6535@o362894.ingest.sentry.io/5319748"});
 
+if(location.href.indexOf('/impersonate') !== -1) {
+    store.dispatch({type: 'USER_LOGOUT'})
+    let query = new URLSearchParams(location.search);
+    if(query.get('token')) {
+        userToken.resetUserInfo()
+        userToken.addTokenInfo({
+            token: query.get('token'),
+            accessToken: null,
+            refresh: null,
+            expires: 9999999999999
+        })
+    }
+}
 ReactDOM.render(<Main store={store}  />, document.getElementById("root"));
--- src/app/pages/Impersonate/Impersonate.tsx
+++ src/app/pages/Impersonate/Impersonate.tsx
@@ -1,26 +1,11 @@
 import React from 'react'
-import { useQuery } from '../../../utils/utils'
 import { useHistory } from 'react-router'
-import { userToken } from '../../utils/token'
-import { store } from '../..'
 
 export const Impersonate = () => {
-
-    let query = useQuery()
     let history = useHistory()
 
     React.useEffect(() => {
-        store.dispatch({type: 'USER_LOGOUT'});
-        if(query.get('token')) {
-            userToken.resetUserInfo()
-            userToken.addTokenInfo({
-                token: query.get('token'),
-                accessToken: null,
-                refresh: null,
-                expires: 9999999999999
-            })
-            history.push('/dashboard')
-        }
+        history.push('/dashboard')
     }, [])
 
     return (
