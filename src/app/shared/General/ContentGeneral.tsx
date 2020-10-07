2045d966ead16010909fb201d25959ceaf415746
Jake Napper <jake.napper@dacast.com>
2020-10-07 15:30:12 +0100

    Sharing component

--- src/app/shared/General/ContentGeneral.tsx
+++ src/app/shared/General/ContentGeneral.tsx
@@ -31,6 +31,7 @@ import { axiosClient } from '../../utils/services/axios/axiosClient';
 import { getKnowledgebaseLink } from '../../constants/KnowledgbaseLinks';
 import { Divider } from '../Common/MiscStyle';
 import { GeneralDetails } from './Details';
+import { GeneralSharing } from './Sharing';
 
 export interface ContentGeneralProps {
     contentType: string;
@@ -232,48 +233,12 @@ export const ContentGeneralPage = (props: ContentGeneralProps) => {
                         setLocalContentDetails={setContentDetails}
                         setEncoderModalOpen={setEncoderModalOpen}
                     />
-                    <div className='col col-12'>
-                        <header className="flex justify-between">
-                            <Text className='col col-12' size={20} weight='med'>Sharing</Text>
-                            <Button sizeButton="xs" typeButton="secondary" onClick={() => setPreviewModalOpen(true)}>Preview</Button>
-                        </header>
-                        <Text className='pt2 col col-12' size={14}>The Embed Code can add content to your website and the Share Link can be shared on social media.</Text>
-
-                        <div className={ClassThirdXsFullMd + "mt2 pr2 flex flex-column"}>
-                            <LinkBoxLabel>
-                                <Text size={14} weight="med">JavaScript Embed Code</Text>
-                            </LinkBoxLabel>
-                            <LinkBox>
-                                <LinkText size={14} weight="reg">
-                                    {`<script id="${userId}-${props.contentType}-${props.contentDetails.id}" width="${props.contentDetails.embedScaling === "responsive" ? "100%" : props.contentDetails.embedSize}" height="100%" src="https://player.dacast.com/js/player.js?contentId=${userId}-${props.contentType}-${props.contentDetails.id}"  class="dacast-video"></script>`}
-                                </LinkText>
-                                <IconStyle className='pointer' id="copyJSEmbedTooltip" onClick={() => { logAmplitudeEvent('embed video js'); updateClipboard(`<script id="${userId}-${props.contentType}-${props.contentDetails.id}" width="${props.contentDetails.embedScaling === "responsive" ? "100%" : props.contentDetails.embedSize}" height="100%" src="https://player.dacast.com/js/player.js?contentId=${userId}-${props.contentType}-${props.contentDetails.id}"  class="dacast-video"></script>`, 'JavaScript Embed Code Copied') } }>file_copy_outlined</IconStyle>
-                                <Tooltip target="copyJSEmbedTooltip">Copy to clipboard</Tooltip>
-                            </LinkBox>
-                        </div>
-                        <div className={ClassThirdXsFullMd + "mt2 pr2 flex flex-column"}>
-                            <LinkBoxLabel>
-                                <Text size={14} weight="med">Iframe Embed Code</Text>
-                            </LinkBoxLabel>
-                            <LinkBox>
-                                <LinkText size={14} weight="reg">
-                                {`<iframe src="https://${process.env.BASE_IFRAME_URL}/${props.contentType}/${userId}/${props.contentDetails.id}" width="${props.contentDetails.embedScaling === "responsive" ? "100%" : props.contentDetails.embedSize}" height="100%" frameborder="0" scrolling="no" allow="autoplay" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`}
-                                </LinkText>
-                                <IconStyle className='pointer' id="copyIframeEmbedTooltip" onClick={() => { logAmplitudeEvent('embed video iframe'); updateClipboard(`<iframe src="https://${process.env.BASE_IFRAME_URL}/${props.contentType}/${userId}/${props.contentDetails.id}" width="${props.contentDetails.embedScaling === "responsive" ? "100%" : props.contentDetails.embedSize}" height="100%" frameborder="0" scrolling="no" allow="autoplay" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`, 'Iframe Embed Code Copied')}}>file_copy_outlined</IconStyle>
-                                <Tooltip target="copyIframeEmbedTooltip">Copy to clipboard</Tooltip>
-                            </LinkBox>
-                        </div>
-                        <div className={ClassThirdXsFullMd + "mt2 pr2 flex flex-column"}>
-                            <LinkBoxLabel>
-                                <Text size={14} weight="med">Share Link</Text>
-                            </LinkBoxLabel>
-                            <LinkBox>
-                                <LinkText size={14} weight="reg">{`https://${process.env.BASE_IFRAME_URL}/${props.contentType}/${userId}/${props.contentDetails.id}`}</LinkText>
-                                <IconStyle className='pointer' id="copyShareLinkTooltip" onClick={() => { logAmplitudeEvent('share video'); updateClipboard(`https://${process.env.BASE_IFRAME_URL}/${props.contentType}/${userId}/${props.contentDetails.id}`, 'Share Link Copied')} }>file_copy_outlined</IconStyle>
-                                <Tooltip target="copyShareLinkTooltip">Copy to clipboard</Tooltip>
-                            </LinkBox>
-                        </div>
-                    </div>
+                    <GeneralSharing 
+                        userId={userId}
+                        contentDetails={props.contentDetails}
+                        contentType={props.contentType}
+                        setPreviewModalOpen={setPreviewModalOpen}
+                    />
                     {   
                         props.contentType === "live" &&
                         <>
