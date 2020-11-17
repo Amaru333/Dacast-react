0268524631a12325087c17df0e3339991b148fe6
Clement Bissonnier <clement.bissonnier@dacast.com>
2020-11-16 16:38:36 -0800

    Removing sentry

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,7 +6,7 @@ import Main from "./main";
 import configureStore from "./redux-flow/configureStore";
 import { globalDefaultState } from "./redux-flow/store";
 import "react-table/react-table.css";
-import * as Sentry from '@sentry/react';
+// import * as Sentry from '@sentry/react';
 import { datadogRum } from '@datadog/browser-rum';
 import { userToken } from './utils/services/token/tokenService';
 
@@ -25,7 +25,7 @@ datadogRum.init({
     trackInteractions: true
 });
 
-Sentry.init({dsn: "https://2e329011118c44b5a76d3670883a6535@o362894.ingest.sentry.io/5319748"});
+// Sentry.init({dsn: "https://2e329011118c44b5a76d3670883a6535@o362894.ingest.sentry.io/5319748"});
 
 if(location.href.indexOf('impersonate') !== -1) {
     store.dispatch({type: 'USER_LOGOUT'})
