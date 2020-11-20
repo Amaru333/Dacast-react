103bdb41817d857fa8c7fcd34caf10603588428c
Jake Napper <jake.napper@dacast.com>
2020-11-20 16:05:16 +0000

    advertising section

--- src/app/shared/Engagement/Advertising.tsx
+++ src/app/shared/Engagement/Advertising.tsx
@@ -1,14 +1,129 @@
 import React from 'react';
 import { Card } from '../../../components/Card/Card';
+import { Button } from '../../../components/FormsComponents/Button/Button';
+import { Modal } from '../../../components/Modal/Modal';
+import { Table } from '../../../components/Table/Table';
+import { Toggle } from '../../../components/Toggle/toggle';
+import { Tooltip } from '../../../components/Tooltip/Tooltip';
 import { Text } from '../../../components/Typography/Text';
+import { ActionIcon, IconContainer, IconStyle } from '../../../shared/Common/Icon';
+import { dataToTimeVideo } from '../../../utils/formatUtils';
+import { capitalizeFirstLetter } from '../../../utils/utils';
+import { getKnowledgebaseLink } from '../../constants/KnowledgbaseLinks';
+import { NewAdModal } from '../../pages/Settings/Interactions/NewAdModal';
+import { Ad, EngagementInfo } from '../../redux-flow/store/Settings/Interactions';
 import { DisabledSection } from '../Common/MiscStyle';
+import { emptyContentListBody } from '../List/emptyContentListState';
+import { AdTableURLContainer, Header } from './EngagementStyle';
 
-export const EngagementAdvertising = () => {
+export const EngagementAdvertising = (props: {stateEngagementSettings: EngagementInfo, localEngagementSettings: EngagementInfo, setLocalEngagementSettings: React.Dispatch<React.SetStateAction<EngagementInfo>>, setSettingsEdited: React.Dispatch<React.SetStateAction<boolean>>, deleteAd: (data: Ad[]) => Promise<void>, handleSectionRevert?: (section: string) => void, lockSection?: (section: string, contentId: string, contentType: string, unlock?: boolean) => Promise<void>, contentId?: string, contentType?: string} ) => {
+
+    const [selectedAd, setSelectedAd] = React.useState<number>(-1)
+    const [newAdModalOpened, setNewAdModalOpened] = React.useState<boolean>(false);
+
+    const handleAdsLockChange = () => {
+        if (!props.localEngagementSettings.adsSettings.locked) {
+            props.handleSectionRevert('ads')
+        } else {
+            props.setSettingsEdited(true)
+            props.lockSection('ads', props.contentId, props.contentType, true).then(() => {
+                props.setLocalEngagementSettings({
+                    ...props.localEngagementSettings,
+                    adsSettings:{
+                        locked: false,
+                        ads: [],
+                        adsEnabled: false
+                    }
+                })
+            })
+        }
+    }
+
+    const newAd = () => {
+        setSelectedAd(-1);
+        setNewAdModalOpened(true)
+    }
+
+    const editAd = (adIndex: number) => {
+        setSelectedAd(adIndex);
+        setNewAdModalOpened(true);
+    }
+
+    const handleAdPosition = (ad: Ad) => {
+        if(ad["ad-type"] === "pre-roll"){
+            return "Start"
+        } else if(ad["ad-type"] === "post-roll"){
+            return "End"
+        } else {
+            return dataToTimeVideo(ad.timestamp).toString()
+        }
+    }
+
+    const advertisingTableHeader = () => {
+        if (props.stateEngagementSettings.adsSettings.ads.length > 0) {
+            return {
+                data: [
+                    { cell: <Text key='advertisingTableHeaderPlacement' size={14} weight='med'>Placement</Text> },
+                    { cell: <Text key='advertisingTableHeaderPosition' size={14} weight='med'>Position</Text> },
+                    { cell: <Text key='advertisingTableHeaderUrl' size={14} weight='med'>Ad URL</Text> },
+                    {
+                        cell: <div key='advertisingTableHeaderButtons' className='right mr2 flex'>
+                            <Button className="sm-show" typeButton='secondary' sizeButton='xs' buttonColor='blue' onClick={() => { newAd() }}>New Ad</Button>
+                        </div>
+                    }
+                ]
+            }
+        } else {
+            return {
+                data: [
+                    {
+                        cell: <div key='advertisingTableHeaderButtons' className='right mr2 flex'>
+                            <Button className="sm-show" typeButton='secondary' sizeButton='xs' buttonColor='blue' onClick={() => { newAd() }}>New Ad</Button>
+                        </div>
+                    }
+                ]
+            }
+        }
+    }
+
+    const advertisingTableBody = () => {
+        return props.stateEngagementSettings.adsSettings.ads.map((item, i) => {
+            return {
+                data: [
+                    <Text key={'advertisingTableBodyPlacement' + item["ad-type"] + i} size={14} weight='med'>{capitalizeFirstLetter(item["ad-type"])}</Text>,
+                    <Text key={'advertisingTableBodyPosition' + item.timestamp + i} size={14} weight='med'>{handleAdPosition(item)}</Text>,
+                    <AdTableURLContainer>
+                        <Text key={'advertisingTableBodyUrl' + item.url + i} size={14} weight='med'>{item.url}</Text>
+                    </AdTableURLContainer>,
+                    <IconContainer className="iconAction" key={'advertisingTableActionButtons' + i.toString()}>
+                        <ActionIcon>
+                            <IconStyle id={'adTableCopy' + i} onClick={() => props.deleteAd(props.stateEngagementSettings.adsSettings.ads.filter(ad => ad !== item))} >delete</IconStyle>
+                            <Tooltip target={'adTableCopy' + i}>Delete</Tooltip>
+                        </ActionIcon>
+                        <ActionIcon>
+                            <IconStyle id={'adTableEdit' + i} onClick={() => editAd(i)}>edit</IconStyle>
+                            <Tooltip target={'adTableEdit' + i}>Edit</Tooltip>
+                        </ActionIcon>
+                    </IconContainer>
+                ]
+            }
+        })
+    }
+
     return(
+        <React.Fragment>  
         <Card className='my2'>
-                    <Text className="pb2" size={20} weight='med'>Advertising</Text>
-                    <DisabledSection settingsEditable={props.interactionsInfos.adsSettings.ads.length > 0}>
-                        <Toggle id='advertisingEnabled' defaultChecked={props.interactionsInfos.adsSettings.adsEnabled} onChange={() => { setInteractionsInfos({ ...interactionInfos, adsSettings: {...interactionInfos.adsSettings, adsEnabled: !interactionInfos.adsSettings.adsEnabled }}); setSettingsEdited(true) }} label='Advertising enabled' />
+        <Header className="mb2">
+                        <div>
+                            <Text size={20} weight='med'>Advertising</Text>
+                        </div>
+                        <IconStyle className='pointer' id="unlockAdSectionTooltip" onClick={() => {handleAdsLockChange()}}>
+                            {!props.localEngagementSettings.adsSettings.locked ? "lock_open" : "lock"}
+                        </IconStyle>
+                        <Tooltip target="unlockAdSectionTooltip">{!props.localEngagementSettings.adsSettings.locked ? "Click to revert Advertising Settings" : "Click to edit Advertising Settings"}</Tooltip>
+                    </Header>
+                    <DisabledSection settingsEditable={props.stateEngagementSettings.adsSettings.ads.length > 0}>
+                        <Toggle id='advertisingEnabled' defaultChecked={props.stateEngagementSettings.adsSettings.adsEnabled} onChange={() => { props.setLocalEngagementSettings({ ...props.localEngagementSettings, adsSettings: {...props.localEngagementSettings.adsSettings, adsEnabled: !props.localEngagementSettings.adsSettings.adsEnabled }}); props.setSettingsEdited(true) }} label='Advertising enabled' />
                     </DisabledSection>
                     <Text className="py2" size={14} weight='reg' color='gray-3'>Ads configured here will apply to all your content and can be overridden individually. Be aware that Mid-roll ads will only play if the video/stream duration is long enough.</Text>
                     <div className='flex mb2'>
@@ -16,11 +131,17 @@ export const EngagementAdvertising = () => {
                         <Text size={14} weight='reg' color='gray-3'>Need help creating Ads? Visit the <a href={getKnowledgebaseLink("Ads")} target="_blank" rel="noopener noreferrer">Knowledge Base</a></Text>
                     </div>
                     <div className="clearfix mb2">
-                        {/* <Button className='xs-show col mb1 col-12' typeButton='primary' sizeButton='xs' buttonColor='blue' onClick={(event) => { event.preventDefault(); setPreviewModalOpen(true) }}>Preview</Button> */}
-                        <Button className="xs-show col col-12" typeButton='secondary' sizeButton='xs' buttonColor='blue' onClick={(event) => { newAd() }}>New Ad</Button>
+                        <Button className="xs-show col col-12" typeButton='secondary' sizeButton='xs' buttonColor='blue' onClick={() => { newAd() }}>New Ad</Button>
                     </div>
-                    <Table id='advertisingTable' headerBackgroundColor="gray-10" header={advertisingTableHeader()} body={props.interactionsInfos.adsSettings.ads.length > 0 ? advertisingTableBody() : emptyContentListBody("Create a new Ad before enabling Advertising")} />
+                    <Table id='advertisingTable' headerBackgroundColor="gray-10" header={advertisingTableHeader()} body={props.stateEngagementSettings.adsSettings.ads.length > 0 ? advertisingTableBody() : emptyContentListBody("Create a new Ad before enabling Advertising")} />
 
                 </Card>
+                <Modal className='x-visible'  hasClose={false} opened={newAdModalOpened} modalTitle={selectedAd === -1 ? "New Ad" : "Edit Ad"} size='small' toggle={() => setNewAdModalOpened(!newAdModalOpened)}>
+                {
+                    newAdModalOpened &&
+                        <NewAdModal {...props} toggle={setNewAdModalOpened} selectedAd={selectedAd} />
+                }
+            </Modal>
+            </React.Fragment>
     )
 }
