26fea0573e3d9136492b3acd4ad4ac8e58411157
Clement Bissonnier <clement.bissonnier@dacast.com>
2021-02-09 17:35:55 -0800

    GA in app for segment

--- src/app/utils/services/segment/segmentService.ts
+++ src/app/utils/services/segment/segmentService.ts
@@ -47,32 +47,38 @@ const page = (name: string) => {
 const track = (name: string, properties: any) => {
     window.analytics.track(
         name, 
-        properties
+        properties,
+        {
+            'Google Analytics': {
+                'ClientId': formatGaCookie()
+            }
+        }
     );
-//     window.ga(function (tracker) {
-//         console.log('cool', tracker)
-// var clientId = tracker.get('clientId');
-// console.log('My GA universal client ID is: ' + clientId);
-// });
-// console.log(getCookie('_ga'))
 }
 
-// function getCookie(cname: string) {
-//     var name = cname + "=";
-//     let test: string[] = []
-//     var decodedCookie = decodeURIComponent(document.cookie);
-//     var ca = decodedCookie.split(';');
-//     for(var i = 0; i <ca.length; i++) {
-//       var c = ca[i];
-//       while (c.charAt(0) == ' ') {
-//         c = c.substring(1);
-//       }
-//       if (c.indexOf(name) == 0) {
-//         test.push(c.substring(name.length, c.length))
-//       }
-//     }
-//     return test;
-//   }
+export function formatGaCookie() {
+    let gaCookie: string = getCookie('_ga')[0]
+
+    gaCookie = gaCookie.substr(6)
+    return gaCookie
+}
+
+export function getCookie(cname: string) {
+    var name = cname + "=";
+    let test: string[] = []
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        test.push(c.substring(name.length, c.length))
+      }
+    }
+    return test;
+  }
 
 export const segmentService = {
     identify,
