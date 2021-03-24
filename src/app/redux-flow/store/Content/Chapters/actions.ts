e9c39010a62790c8ffba30e94607a3206faa46f8
Clement Bissonnier <clement.bissonnier@dacast.com>
2021-03-23 18:25:55 -0700

    Chapter markers view model

--- src/app/redux-flow/store/Content/Chapters/actions.ts
+++ src/app/redux-flow/store/Content/Chapters/actions.ts
@@ -1,83 +1,63 @@
 import { ActionTypes } from "./types"
-import { ThunkDispatch } from "redux-thunk"
-import { ApplicationState } from "../.."
-import { ContentChaptersServices } from './services'
-import { showToastNotification } from '../../Toasts'
 import { ChapterMarker } from './types'
-import { parseContentType } from '../../../../utils/utils'
+import { applyViewModel } from '../../../../utils/utils'
+import { dacastSdk } from "../../../../utils/services/axios/axiosClient"
+import { ContentType } from "../../Common/types"
+import { formatGetVodChapterMarkersInput, formatGetVodChapterMarkersOutput, formatPutVodChapterMarkersInput, formatPutVodChapterMarkersOutput } from "./viewModel"
 
 export interface GetContentChapterMarkers {
     type: ActionTypes.GET_CONTENT_CHAPTER_MARKERS;
-    payload: {contentId: string; contentType: string; data: { chapterMarkers: ChapterMarker[]}};
+    payload: {contentId: string; contentType: ContentType; chapterMarkers: ChapterMarker[]};
 }
 
 export interface SaveContentChapterMarker {
     type: ActionTypes.SAVE_CONTENT_CHAPTER_MARKER;
-    payload: {contentId: string; contentType: string; data: ChapterMarker[]};
+    payload: {contentId: string; contentType: ContentType; chapterMarkers: ChapterMarker[]};
 }
 
 export interface AddContentChapterMarker {
     type: ActionTypes.ADD_CONTENT_CHAPTER_MARKER;
-    payload: {contentId: string; contentType: string; data: ChapterMarker[]};
+    payload: {contentId: string; contentType: ContentType; chapterMarkers: ChapterMarker[]};
 }
 
 export interface DeleteContentChapterMarker {
     type: ActionTypes.DELETE_CONTENT_CHAPTER_MARKER;
-    payload: {contentId: string; contentType: string; data: ChapterMarker[]};
+    payload: {contentId: string; contentType: ContentType; chapterMarkers: ChapterMarker[]};
 }
 
-export const getContentChapterMarkersAction = (contentId: string, contentType: string): ThunkDispatch<Promise<void>, {}, GetContentChapterMarkers> => {
-    return async (dispatch: ThunkDispatch<ApplicationState , {}, GetContentChapterMarkers> ) => {
-        await ContentChaptersServices.getContentChapterMarkersService(contentId, parseContentType(contentType))
-            .then( response => {
-                dispatch( {type: ActionTypes.GET_CONTENT_CHAPTER_MARKERS, payload:{contentId: contentId, contentType: contentType, data: response.data.data} } )
-            })
-            .catch(() => {
-                dispatch(showToastNotification("Oops! Something went wrong..", 'fixed', "error"))
-                return Promise.reject()
-            })
+export const getContentChapterMarkersAction = (contentType: ContentType) => {
+    switch(contentType) {
+        case 'vod': 
+            return applyViewModel(dacastSdk.getVodChapterMarkers, formatGetVodChapterMarkersInput, formatGetVodChapterMarkersOutput(contentType), ActionTypes.GET_CONTENT_CHAPTER_MARKERS, null, 'Couldn\'t get chapter markers')
+        default:
+            throw new Error('Error applying get content view model')
     }
 }
 
-export const saveContentChapterMarkerAction = (contentId: string, contentType: string, data: ChapterMarker[]): ThunkDispatch<Promise<void>, {}, SaveContentChapterMarker> => {
-    return async (dispatch: ThunkDispatch<ApplicationState , {}, SaveContentChapterMarker> ) => {
-        await ContentChaptersServices.saveContentChapterMarkerService(contentId, parseContentType(contentType), data)
-            .then( () => {
-                dispatch( {type: ActionTypes.SAVE_CONTENT_CHAPTER_MARKER, payload:{contentId: contentId, contentType: contentType, data: data} } )
-                dispatch(showToastNotification(`Chapter has been saved`, 'fixed', "success"))
-            })
-            .catch(() => {
-                dispatch(showToastNotification("Oops! Something went wrong..", 'fixed', "error"))
-                return Promise.reject()
-            })
+export const saveContentChapterMarkerAction = (contentType: ContentType) => {
+    switch(contentType) {
+        case 'vod': 
+            return applyViewModel(dacastSdk.putVodChapterMarkers, formatPutVodChapterMarkersInput, formatPutVodChapterMarkersOutput(contentType), ActionTypes.SAVE_CONTENT_CHAPTER_MARKER, 'Chapter has been saved', 'Couldn\'t save chapter')
+        default:
+            throw new Error('Error applying get content view model')
     }
 }
 
-export const addContentChapterMarkerAction = (contentId: string, contentType: string, data: ChapterMarker[]): ThunkDispatch<Promise<void>, {}, AddContentChapterMarker> => {
-    return async (dispatch: ThunkDispatch<ApplicationState , {}, AddContentChapterMarker> ) => {
-        await ContentChaptersServices.saveContentChapterMarkerService(contentId, parseContentType(contentType), data)
-            .then( () => {
-                dispatch( {type: ActionTypes.ADD_CONTENT_CHAPTER_MARKER, payload:{contentId: contentId, contentType: contentType, data: data} } )
-                dispatch(showToastNotification(`Chapter has been saved`, 'fixed', "success"))
-            })
-            .catch(() => {
-                dispatch(showToastNotification("Oops! Something went wrong..", 'fixed', "error"))
-                return Promise.reject()
-            })
+export const addContentChapterMarkerAction = (contentType: ContentType) => {
+    switch(contentType) {
+        case 'vod': 
+            return applyViewModel(dacastSdk.putVodChapterMarkers, formatPutVodChapterMarkersInput, formatPutVodChapterMarkersOutput(contentType), ActionTypes.ADD_CONTENT_CHAPTER_MARKER, 'Chapter has been saved', 'Couldn\'t save chapter')
+        default:
+            throw new Error('Error applying get content view model')
     }
 }
 
-export const deleteContentChapterMarkerAction = (contentId: string, contentType: string, data: ChapterMarker[]): ThunkDispatch<Promise<void>, {}, DeleteContentChapterMarker> => {
-    return async (dispatch: ThunkDispatch<ApplicationState , {}, DeleteContentChapterMarker> ) => {
-        await ContentChaptersServices.saveContentChapterMarkerService(contentId, parseContentType(contentType), data)
-            .then( () => {
-                dispatch( {type: ActionTypes.DELETE_CONTENT_CHAPTER_MARKER, payload:{contentId: contentId, contentType: contentType, data: data} } )
-                dispatch(showToastNotification(`Chapter has been deleted`, 'fixed', "success"))
-            })
-            .catch(() => {
-                dispatch(showToastNotification("Oops! Something went wrong..", 'fixed', "error"))
-                return Promise.reject()
-            })
+export const deleteContentChapterMarkerAction = (contentType: ContentType) => {
+    switch(contentType) {
+        case 'vod': 
+            return applyViewModel(dacastSdk.putVodChapterMarkers, formatPutVodChapterMarkersInput, formatPutVodChapterMarkersOutput(contentType), ActionTypes.SAVE_CONTENT_CHAPTER_MARKER, 'Chapter has been deleted', 'Couldn\'t delete chapter')
+        default:
+            throw new Error('Error applying get content view model')
     }
 }
 
