886226281f465a7ae43d0285d86b31d9c703964c
Lawrence Sears <lawrence@lawrencesears.com>
2021-04-16 16:25:44 +0100

    (DC-2741) Adds admin button for forcing inPlayer account setup

--- src/DacastSdk/sdk.ts
+++ src/DacastSdk/sdk.ts
@@ -62,7 +62,8 @@ export class DacastSdk {
     public postAccountTransaction = async (input: PostAccountTransactionInput): Promise<void> => await this.axiosClient.post('/paywall-transactions', input)
     public getPirateInfo = async (input: string): Promise<GetPirateInfoOutput> => await this.axiosClient.get('/identify-cdn-url?url=' + input).then(this.checkExtraData)
     public putExtendTrial = async (input: PutExtendTrialInput): Promise<void> => await this.axiosClient.put('/accounts/' + input.userId + '/extend-trial', input.payload)
-    
+    public postForceInplayerSetup = async (salesforceID: string): Promise<void> => await this.axiosClient.post(`/accounts/${salesforceID}/force-inplayer-setup`)
+
     public getJobsList = async (): Promise<GetJobsListOutput> => await this.axiosClient.get('/migration/jobs').then(this.checkExtraData)
     public getJobDetails = async (input: string): Promise<GetMigrationJobDetailsOutput> => await this.axiosClient.get('/migration/job/' + input).then(this.checkExtraData)
     public postStartMigrationJob = async (input: PostStartMigrationJobInput) => await this.axiosClient.post('/migration/job', input)
