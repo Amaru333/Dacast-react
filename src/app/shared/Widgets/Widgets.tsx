7f7caec3160dac27826260e777c8443aa81bd441
Nestor Turizo <turizoft@gmail.com>
2021-04-26 16:30:10 -0500

    update data remaining buttons color on dashboard

--- src/app/shared/Widgets/Widgets.tsx
+++ src/app/shared/Widgets/Widgets.tsx
@@ -9,11 +9,11 @@ export const handleButtonToPurchase = (percentage: number, purchaseItem: string,
 
     if(percentage <= 25 ) {
         return (
-            <Text className="ml-auto" size={12} weight="med" color="dark-violet"> <Button buttonColor="blue" sizeButton="xs" onClick={() => history.push("/account/upgrade")}>Upgrade</Button></Text>
+            <Text className="ml-auto" size={12} weight="med" color="dark-violet"> <Button buttonColor="lightBlue" sizeButton="xs" onClick={() => history.push("/account/upgrade")}>Upgrade</Button></Text>
         )
     } else {
         return (
-            <Text className="ml-auto" size={12} weight="med" color="dark-violet"> <Button buttonColor={percentage <= 25 ? "red" : "blue"} sizeButton="xs" onClick={() => planPage ? callback() : history.push("/account/plan#purchase-data")}>Buy More</Button></Text>
+            <Text className="ml-auto" size={12} weight="med" color="dark-violet"> <Button buttonColor={percentage <= 25 ? "red" : "lightBlue"} sizeButton="xs" onClick={() => planPage ? callback() : history.push("/account/plan#purchase-data")}>Buy More</Button></Text>
         )
     }
 }
