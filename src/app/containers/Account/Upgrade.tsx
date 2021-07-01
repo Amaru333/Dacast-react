fe5f4dc583a0cc1b707a1d5ee2f686ae897468d7
Nestor Turizo <turizoft@gmail.com>
2021-06-30 23:43:05 -0500

    add unified pricing iframe

--- src/app/containers/Account/Upgrade.tsx
+++ src/app/containers/Account/Upgrade.tsx
@@ -12,15 +12,17 @@ import { BillingPageInfos } from '../../redux-flow/store/Account/Plan/types';
 import { ErrorPlaceholder } from '../../../components/Error/ErrorPlaceholder';
 import { CompanyPageInfos, getCompanyPageDetailsAction } from '../../redux-flow/store/Account/Company';
 import { userToken } from '../../utils/services/token/tokenService';
-
+import { showToastNotification, hideToast } from '../../redux-flow/store/Toasts';
+import { Size, NotificationType, NotificationPosition } from '../../../components/Toast/ToastTypes';
 
 export interface UpgradeContainerProps {
     planDetails: Plans;
     billingInfos: BillingPageInfos;
     companyInfo: CompanyPageInfos;
     getPlanDetails: () => Promise<void>;
-    getBillingPageInfos: () => Promise<void>
-    getCompanyInfo: () => Promise<void>
+    getBillingPageInfos: () => Promise<void>;
+    getCompanyInfo: () => Promise<void>;
+    showDiscardToast: (text: string, size: Size, notificationType: NotificationType) => void;
 }
 
 const UpgradeContainer = (props: UpgradeContainerProps) => {
@@ -30,11 +32,11 @@ const UpgradeContainer = (props: UpgradeContainerProps) => {
     React.useEffect(() => {
         if(userToken.getPrivilege('privilege-billing')) {
             props.getBillingPageInfos()
-            .catch(() => setNodataFetched(true))
+                .catch(() => setNodataFetched(true))
         }
 
         props.getPlanDetails()
-        .catch(() => setNodataFetched(true))
+            .catch(() => setNodataFetched(true))
 
         if(!props.companyInfo) {
             props.getCompanyInfo()
@@ -46,9 +48,9 @@ const UpgradeContainer = (props: UpgradeContainerProps) => {
     }
 
     return (
-        props.planDetails ? 
+        props.planDetails ?
             <UpgradePage planDetails={props.planDetails} billingInfos={props.billingInfos} {...props}/>
-            : 
+            :
             <SpinnerContainer><LoadingSpinner size='medium' color='violet' /></SpinnerContainer>
     )
 }
@@ -71,6 +73,12 @@ export function mapDispatchToProps(dispatch: ThunkDispatch<ApplicationState, voi
         },
         getCompanyInfo: async () => {
             await dispatch(getCompanyPageDetailsAction(undefined))
+        },
+        showToast: (text: string, size: Size, notificationType: NotificationType, permanent?: boolean, position?: NotificationPosition) => {
+            return dispatch(showToastNotification(text, size, notificationType, permanent, position))
+        },
+        hideToast: (toast: ToastType) => {
+            return dispatch(hideToast(toast))
         }
     }
 }
