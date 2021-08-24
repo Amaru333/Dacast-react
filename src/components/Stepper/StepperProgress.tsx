abc781e188179558ada4f3f24167dc0b9b373ea9
Nestor Turizo <turizoft@gmail.com>
2021-08-23 21:03:01 -0500

    update icons for custom stepper progress bar

--- src/components/Stepper/StepperProgress.tsx
+++ src/components/Stepper/StepperProgress.tsx
@@ -1,10 +1,9 @@
-//REPLACE THIS WITH SVG LATER
-import Icon from '@material-ui/core/Icon';
 import React from 'react';
+import { IconStyle } from '../../shared/Common/Icon';
 import { EmptyProgressBar, StepperProgressBar, StepperProgressContainer, StepperProgressWrapper, StepTitle, StepTitleNumber } from './StepperStyles';
 import { Step } from './StepperTypes';
 
-export const StepperProgress = (props: {stepList: Step[], currentStep: number}) => {
+export const StepperProgress = (props: {stepList: Step[]; currentStep: number}) => {
 
     const progressPerStep = 100 / (props.stepList.length - 1);
     const progress = props.currentStep * progressPerStep;
@@ -18,7 +17,7 @@ export const StepperProgress = (props: {stepList: Step[], currentStep: number}) 
                     return (
                         <StepTitle isCurrentStep={i === props.currentStep}>
                             <StepTitleNumber isFutureStep={i > props.currentStep}>
-                                {i < props.currentStep ? <Icon fontSize="small" style={{color: "white"}}>check</Icon> :  (i + 1)}
+                                {i < props.currentStep ? <IconStyle fontSize="small" coloricon="white">check</IconStyle> :  (i + 1)}
                             </StepTitleNumber>
                             {step.title}
                         </StepTitle>
