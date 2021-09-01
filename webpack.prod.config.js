f54d3c7f3892bf3a0de3487d88e6d89244b82051
Clement Bissonnier <clement.bissonnier@dacast.com>
2021-09-01 11:03:32 -0700

    webpack config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -95,6 +95,7 @@ module.exports = {
         new webpack.HotModuleReplacementPlugin(),
         new CopyWebpackPlugin([
             { from: './public/iframe', to: './public/iframe' },
+            { from: './public/locales', to: './public/locales' },
         ], { copyUnmodified: true }
         )
     ]
